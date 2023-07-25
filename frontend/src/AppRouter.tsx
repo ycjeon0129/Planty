8efed82ef783a1fbf66c2f6802e4c1d38a7054ee
Im-hass <huiseon4107@gmail.com>
2023-07-25 17:59:36 +0900

    [FE] Style : 페이지 레이아웃 설정

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -3,15 +3,18 @@ import HomePage from 'pages/Home/HomePage';
 import React from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './styles/index.scss';
+import PageLayout from './components/layout/PageLayout';
 
 function AppRouter() {
 	return (
 		<BrowserRouter>
-			<Navigation />
-			<Routes>
-				<Route path="/" element={<Navigate replace to="/home" />} />
-				<Route path="/home" element={<HomePage />} />
-			</Routes>
+			<PageLayout>
+				<Navigation />
+				<Routes>
+					<Route path="/" element={<Navigate replace to="/home" />} />
+					<Route path="/home" element={<HomePage />} />
+				</Routes>
+			</PageLayout>
 		</BrowserRouter>
 	);
 }
