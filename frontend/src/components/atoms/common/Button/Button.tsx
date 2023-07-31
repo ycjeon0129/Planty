1d3c8f1b1fc5a95479cefbc74655a8f16b4cf6e7
전인혁 <wjs_5025@naver.com>
2023-07-31 21:07:26 +0900

    [FE] Feat : 공통 컴포넌트 개발(버튼)

--- frontend/src/components/atoms/common/Button/Button.tsx
+++ frontend/src/components/atoms/common/Button/Button.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import classnames from 'classnames';
+import './Button.scss';
 
-function Button() {
-	return <div>Button</div>;
+function Button({ isActive, message, onClick }: { isActive: boolean; message: string; onClick: () => void }) {
+	const className = classnames('button-container', { primary: isActive, disabled: !isActive });
+	return (
+		<div className={className} onClick={onClick} role="presentation">
+			{message}
+		</div>
+	);
 }
 
 export default Button;
