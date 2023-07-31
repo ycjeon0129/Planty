f6d4d32de2951e4faa929947826d53218e483108
전인혁 <wjs_5025@naver.com>
2023-07-31 20:58:59 +0900

    [FE] Feat : 공통 컴포넌트 구현(구독 목록 아이템)

--- frontend/src/components/organisms/subscribe/SubscribeListItem/SubscribeListItem.tsx
+++ frontend/src/components/organisms/subscribe/SubscribeListItem/SubscribeListItem.tsx
@@ -4,13 +4,13 @@ import React from 'react';
 import SubscribeItemLayout from 'components/layout/subscirbe/SubscribeListItemLayout/SubscribeListItemLayout';
 import ListItemHeader from 'components/atoms/common/ListItemHeader/ListItemHeader';
 import SubscribeStateBadge from 'components/atoms/subscribe/SubscribeStateBadge/SubscribeStateBadge';
-import ListItemInfoList from 'components/organisms/common/ListItemInfoList/ListItemInfoList';
 import Button from 'components/atoms/common/Button/Button';
+import InfoList from 'components/organisms/common/ListItemInfoList/InfoList';
 
 function SubscribeListItem() {
 	// dumy
 	const test = {
-		menu: 'ㅎㅇ',
+		구독시작일: '2022-07-23',
 		menu1: 'ㅎㅇ1',
 		menu2: 'ㅎㅇ2',
 	};
@@ -18,13 +18,13 @@ function SubscribeListItem() {
 	return (
 		<SubscribeItemLayout>
 			<ListItemHeader title="누구나 쉽게 키우는 몬스테라 클래스" url="url" />
-			<SubscribeStateBadge stateKey="wait" />
+			<SubscribeStateBadge stateKey="done" />
 			<img
 				src="https://i.ytimg.com/vi/xeCsFtwVozo/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhWIGAoZTAP&rs=AOn4CLDonoxD_GnPWvni23yD6jOIMWIKuQ"
 				alt=""
 			/>
-			<ListItemInfoList info={test} />
-			<Button />
+			<InfoList info={test} />
+			<Button isActive message="컨설팅 이용하기" />
 		</SubscribeItemLayout>
 	);
 }
