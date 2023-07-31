23f3466c2df8fabd879485a4ada2067f6b910d35
GoldGyu <lkg2097@naver.com>
2023-07-31 17:59:36 +0900

    [FE] Refactor : 코드 각색

--- frontend/src/components/atoms/common/ToolBar/ToolMenuBar.tsx
+++ frontend/src/components/atoms/common/ToolBar/ToolMenuBar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './ToolMenuBar.scss';
+import homeImg from 'assets/icons/ToolBar/HomeImg.svg';
 
 function ToolMenuBar() {
 	return (
 		<div className="menuBox">
 			<div>
-				<div className="iconBox">이미지</div>
+				<div className="iconBox">
+					<img src={homeImg} alt="홈" />
+				</div>
 				<span>홈</span>
 			</div>
 		</div>
--- frontend/src/components/organisms/common/ToolBarOrg/ToolBarOrg.tsx
+++ frontend/src/components/organisms/common/ToolBarOrg/ToolBarOrg.tsx
@@ -3,10 +3,21 @@ import ToolMenuBar from 'components/atoms/common/ToolBar/ToolMenuBar';
 import 'components/organisms/common/ToolBarOrg/ToolBarOrg.scss';
 
 function ToolBarOrg() {
+	// const ImgLst = [
+	// 	'assets/icons/ToolBar/HomeImg.svg',
+	// 	'assets/icons/ToolBar/SubscribeImg.svg',
+	// 	'assets/icons/ToolBar/EmergencyImg.svg',
+	// 	'assets/icons/ToolBar/MypageImg.svg',
+	// ]
+
+	// const ImgName: string = ['홈', '구독샵', '응급실', '마이페이지'];
 	return (
 		<div>
 			<div>ToolBarorganism</div>
 			<div className="ToolBarOrgBox">
+				<ToolMenuBar />
+				<ToolMenuBar />
+				<ToolMenuBar />
 				<ToolMenuBar />
 			</div>
 		</div>
