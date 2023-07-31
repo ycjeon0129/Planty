759915568f958b1daa4e26441d659289ba29f0fd
임희선 <huiseon4107@gmail.com>
2023-08-01 01:25:04 +0900

    [FE] Chore : 페이지 레이아웃 선언 위치 변경 AppRouter.tsx -> HomePage.tsx

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -2,25 +2,17 @@ import HomePage from 'pages/Home/HomePage';
 import React from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './styles/index.scss';
-import NavigationLayout from 'components/layout/NavigationLayout/NavigationLayout';
-import Navigation from 'components/organisms/common/Navigation/Navigation';
 import Develop from 'pages/Develop';
-import PageLayout from './components/layout/PageLayout/PageLayout';
 
 function AppRouter() {
 	return (
 		<BrowserRouter>
-			<PageLayout>
-				<NavigationLayout>
-					<Navigation />
-				</NavigationLayout>
-				<Routes>
-					<Route path="/" element={<Navigate replace to="/home" />} />
-					<Route path="/home" element={<HomePage />} />
-					{/* 컴포넌트 개발용 */}
-					<Route path="/develop" element={<Develop />} />
-				</Routes>
-			</PageLayout>
+			<Routes>
+				<Route path="/" element={<Navigate replace to="/home" />} />
+				<Route path="/home" element={<HomePage />} />
+				{/* 컴포넌트 개발용 */}
+				<Route path="/develop" element={<Develop />} />
+			</Routes>
 		</BrowserRouter>
 	);
 }
--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import NavigationLayout from 'components/layout/NavigationLayout/NavigationLayout';
+import PageLayout from 'components/layout/PageLayout/PageLayout';
+import Navigation from 'components/organisms/common/Navigation/Navigation';
 
 function HomePage() {
-	return <div>HomePage</div>;
+	return (
+		<PageLayout>
+			<NavigationLayout>
+				<Navigation />
+			</NavigationLayout>
+		</PageLayout>
+	);
 }
 
 export default HomePage;
