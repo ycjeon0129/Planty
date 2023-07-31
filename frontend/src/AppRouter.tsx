0ec996765937513c340cad09aa52079d15677ee6
wjs_5025 <wjs_5025>
2023-07-31 13:21:55 +0900

    [FE] Add : 컴포넌트 개발용 페이지 추가

--- frontend/src/AppRouter.tsx
+++ frontend/src/AppRouter.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './styles/index.scss';
 import NavigationLayout from 'components/layout/NavigationLayout/NavigationLayout';
 import Navigation from 'components/ui/Navigation';
+import Develop from 'pages/Develop';
 import PageLayout from './components/layout/PageLayout/PageLayout';
 
 function AppRouter() {
@@ -16,6 +17,8 @@ function AppRouter() {
 				<Routes>
 					<Route path="/" element={<Navigate replace to="/home" />} />
 					<Route path="/home" element={<HomePage />} />
+					{/* 컴포넌트 개발용 */}
+					<Route path="/develop" element={<Develop />} />
 				</Routes>
 			</PageLayout>
 		</BrowserRouter>
---
+++ frontend/src/pages/Develop.tsx
@@ -0,0 +1,12 @@
+import SubscribeStateBadge from 'components/atoms/subscribe/SubscribeStateBadge/SubscribeStateBadge';
+import React from 'react';
+
+function Develop() {
+	return (
+		<div>
+			<SubscribeStateBadge stateKey="wait" />
+		</div>
+	);
+}
+
+export default Develop;
