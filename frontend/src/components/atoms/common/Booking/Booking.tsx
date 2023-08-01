d6f946fa1334c2ee0f3543e88bff725698b572e3
Gold-gyu <lkg2097@naver.com>
2023-08-02 00:24:21 +0900

    [FE] Feat : 예약시간 공통 컴포넌트 구현 완료

---
+++ frontend/src/components/atoms/common/Booking/Booking.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './Booking.scss'; // 스타일 파일을 불러옵니다.
+
+interface BookingProps {
+	isActivate: boolean;
+	time: string;
+}
+
+function Booking(props: BookingProps) {
+	const { isActivate, time } = props;
+	const [isActive, setIsActive] = useState(false);
+
+	const handleButtonClick = () => {
+		if (isActivate) {
+			setIsActive(!isActive); // isActive 값을 토글합니다.
+		}
+	};
+
+	return (
+		<div>
+			{/* 회색 버튼 (예약할 수 없는 비활성화 버튼) */}
+			{isActive ? (
+				<div className="select" onClick={handleButtonClick} role="presentation">
+					<p>{time}</p>
+				</div>
+			) : (
+				<div className={isActivate ? 'active' : 'box'} onClick={handleButtonClick} role="presentation">
+					<p>{time}</p>
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default Booking;
