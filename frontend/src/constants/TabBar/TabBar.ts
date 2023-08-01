8538d2c7db7b40df7158167fb4634015f2df8698
GoldGyu <lkg2097@naver.com>
2023-08-01 12:01:14 +0900

    [FE] Chore : svg 경로 수정 (기존 ToolBar - 수정후 TabBar)

--- frontend/src/constants/TabBar/TabBar.ts
+++ frontend/src/constants/TabBar/TabBar.ts
@@ -7,19 +7,19 @@ interface ToolMenuBarProps {
 
 const ToolMenu: ToolMenuBarProps = {
 	Home: {
-		imgSrc: '../../../../assets/icons/ToolBar/HomeImg.svg',
+		imgSrc: '../../../../assets/icons/tabbar/HomeImg.svg',
 		imgName: '홈',
 	},
 	Subscribe: {
-		imgSrc: '../../../../assets/icons/ToolBar/SubscribeImg.svg',
+		imgSrc: '../../../../assets/icons/tabbar/SubscribeImg.svg',
 		imgName: '구독샵',
 	},
 	Emergency: {
-		imgSrc: '../../../../assets/icons/ToolBar/EmergencyImg.svg',
+		imgSrc: '../../../../assets/icons/tabbar/EmergencyImg.svg',
 		imgName: '응급실',
 	},
 	Mypage: {
-		imgSrc: '../../../../assets/icons/ToolBar/MypageImg.svg',
+		imgSrc: '../../../../assets/icons/tabbar/MypageImg.svg',
 		imgName: '마이페이지',
 	},
 };
