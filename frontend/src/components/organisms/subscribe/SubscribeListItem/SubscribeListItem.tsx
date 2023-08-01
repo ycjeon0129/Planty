ffa62f4d2e79760c2caa37a6749d8b71d06b9e6d
wjs_5025 <wjs_5025>
2023-08-01 10:18:06 +0900

    [FE] Chore : 컴포넌트명 변경(ListItemHeader -> ListItemTitle)

--- frontend/src/components/organisms/subscribe/SubscribeListItem/SubscribeListItem.tsx
+++ frontend/src/components/organisms/subscribe/SubscribeListItem/SubscribeListItem.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SubscribeItemLayout from 'components/layout/subscirbe/SubscribeListItemLayout/SubscribeListItemLayout';
-import ListItemHeader from 'components/atoms/common/ListItemHeader/ListItemHeader';
+import ListItemTitle from 'components/atoms/common/ListItemTitle/ListItemTitle';
 import SubscribeStateBadge from 'components/atoms/subscribe/SubscribeStateBadge/SubscribeStateBadge';
 import Button from 'components/atoms/common/Button/Button';
 import InfoList from 'components/organisms/common/InfoList/InfoList';
@@ -19,7 +19,7 @@ function SubscribeListItem() {
 
 	return (
 		<SubscribeItemLayout>
-			<ListItemHeader title="누구나 쉽게 키우는 몬스테라 클래스" url="url" />
+			<ListItemTitle title="누구나 쉽게 키우는 몬스테라 클래스" url="url" />
 			<SubscribeStateBadge stateKey="wait" />
 			<img
 				src="https://i.ytimg.com/vi/xeCsFtwVozo/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhWIGAoZTAP&rs=AOn4CLDonoxD_GnPWvni23yD6jOIMWIKuQ"
