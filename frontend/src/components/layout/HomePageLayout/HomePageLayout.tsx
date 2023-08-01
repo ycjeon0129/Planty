2579a7fa09cde6cb29a033095615178da3d6d68d
wjs_5025 <wjs_5025>
2023-08-01 15:05:54 +0900

    [FE] Design : HomePage 레이아웃 수정

--- frontend/src/components/layout/HomePageLayout/HomePageLayout.tsx
+++ frontend/src/components/layout/HomePageLayout/HomePageLayout.tsx
@@ -1,26 +1,30 @@
 import React, { ReactNode } from 'react';
 import './HomePageLayout.scss';
+import ContentsLayout from '../ContentsLayout/ContentsLayout';
+import PageLayout from '../PageLayout/PageLayout';
 
 function HomePageLayout({ children }: { children: ReactNode[] }) {
 	return (
-		<div className="home-page-layout-container">
-			{/* 배너 */}
-			<div id="banner">{children[0]}</div>
-			{/* 내 구독정보 */}
-			<div id="my-subcribe">
-				{children[1]}
-				{children[2]}
+		<PageLayout>
+			<div className="home-page-layout-container">
+				{/* 배너 */}
+				<ContentsLayout id="banner">{children[0]}</ContentsLayout>
+				{/* 내 구독정보 */}
+				<ContentsLayout id="my-subcribe">
+					{children[1]}
+					{children[2]}
+				</ContentsLayout>
+				{/* 바로가기 메뉴 */}
+				<ContentsLayout id="menu">
+					<div id="booking">{children[3]}</div>
+					<div id="emergency">
+						{children[4]}
+						{children[5]}
+					</div>
+				</ContentsLayout>
+				{/* 추천 구독 상품 */}
 			</div>
-			{/* 바로가기 메뉴 */}
-			<div id="menu">
-				<div id="booking">{children[3]}</div>
-				<div id="emergency">
-					{children[4]}
-					{children[5]}
-				</div>
-			</div>
-			{/* 추천 구독 상품 */}
-		</div>
+		</PageLayout>
 	);
 }
 
