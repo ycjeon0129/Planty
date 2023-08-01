1bcf24e5fe6ee853fdbefd8ce7b498d7abbc53d4
전인혁 <wjs_5025>
2023-08-01 13:07:15 +0900

    [FE] Fix : import 경로 수정

--- frontend/src/constants/TabBar/TabBar.ts
+++ frontend/src/constants/TabBar/TabBar.ts
@@ -7,19 +7,19 @@ interface ToolMenuBarProps {
 
 const ToolMenu: ToolMenuBarProps = {
 	Home: {
-		imgSrc: '../../../../assets/icons/tabbar/HomeImg.svg',
+		imgSrc: 'assets/icons/tabbar/HomeImg.svg',
 		imgName: '홈',
 	},
 	Subscribe: {
-		imgSrc: '../../../../assets/icons/tabbar/SubscribeImg.svg',
+		imgSrc: 'assets/icons/tabbar/SubscribeImg.svg',
 		imgName: '구독샵',
 	},
 	Emergency: {
-		imgSrc: '../../../../assets/icons/tabbar/EmergencyImg.svg',
+		imgSrc: 'assets/icons/tabbar/EmergencyImg.svg',
 		imgName: '응급실',
 	},
 	Mypage: {
-		imgSrc: '../../../../assets/icons/tabbar/MypageImg.svg',
+		imgSrc: 'ssets/icons/tabbar/MypageImg.svg',
 		imgName: '마이페이지',
 	},
 };
