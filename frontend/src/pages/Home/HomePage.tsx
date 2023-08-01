801eec4562ab786c3c7aaee6b1e44a4908b1d4e5
wjs_5025 <wjs_5025>
2023-08-01 15:05:38 +0900

    [FE] Feat : 홈 컴포넌트 배치

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,12 +1,15 @@
 import HomePageLayout from 'components/layout/HomePageLayout/HomePageLayout';
+import AreaTitle from 'components/atoms/common/AreaTitle/AreaTitle';
+import SubscribeSlider from 'components/organisms/subscribe/SubscribeSlider/SubscribeSlider';
 import React from 'react';
+import BannerSlider from 'components/organisms/common/BannerSlider/BannerSlider';
 
 function HomePage() {
 	return (
 		<HomePageLayout>
-			<div>banner</div>
-			<div>내 구독 정보</div>
-			<div>구독 아이템</div>
+			<BannerSlider />
+			<AreaTitle title="내 구독 정보" url="subscribe" />
+			<SubscribeSlider />
 			<div>예약관리</div>
 			<div>채팅 응급실</div>
 			<div>화상 응급실</div>
