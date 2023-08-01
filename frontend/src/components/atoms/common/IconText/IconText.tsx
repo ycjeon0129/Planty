3525f567f6acab8f28693816c4da59a73253efa1
임희선 <huiseon4107@gmail.com>
2023-08-01 22:14:44 +0900

    [FE] Design : flex 관련 믹스인 파일 분리

--- frontend/src/components/atoms/common/IconText/IconText.tsx
+++ frontend/src/components/atoms/common/IconText/IconText.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './IconText.scss';
-import ICON_LIST from 'constants/menu/MypageMenuState';
 
 /**
  * 아이콘과 텍스트 컴포넌트
  * @param text string, 아이콘 옆에 있는 텍스트
  */
-function IconText({ text }: { text: string }) {
+function IconText({ url, text }: { url: string; text: string }) {
 	return (
 		<div className="icon-text-container">
 			<div className="img-wrap">
-				<img src={ICON_LIST[text]} alt={text} height={14} width={14} />
+				<img src={url} alt={text} height={14} width={14} />
 			</div>
 			<p className="text">{text}</p>
 		</div>
