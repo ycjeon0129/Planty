210e328d72bf1b4a4e97065e8e7b4a225600f6a9
임희선 <huiseon4107@gmail.com>
2023-08-01 20:45:22 +0900

    [FE] Chore : 컴포넌트에 주석 추가

--- frontend/src/components/atoms/common/HeaderButton/HeaderButton.tsx
+++ frontend/src/components/atoms/common/HeaderButton/HeaderButton.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './HeaderButton.scss';
 
+/**
+ * 뒤로가기, 닫기 버튼이 있는 헤더 컴포넌트
+ * @param icon string, 아이콘 url
+ * @param text string, 버튼 옆에 있는 텍스트
+ */
 function HeaderButton({ icon, text }: { icon: string; text: string }) {
 	return (
 		<div className="header-button">
--- frontend/src/components/atoms/common/PageTitle/PageTitle.tsx
+++ frontend/src/components/atoms/common/PageTitle/PageTitle.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './PageTitle.scss';
 
+/**
+ * 페이지 제목 컴포넌트
+ * @param icon string, 아이콘 url
+ * @param text string, 아이콘 옆에 있는 텍스트
+ */
 function PageTitle({ icon, text }: { icon: string; text: string }) {
 	return (
-		<div className="page-title">
+		<div className="page-title-container">
 			<div className="icon-wrap">
 				<img src={icon} alt={text} height={14} width={14} />
 			</div>
