4776afc4a9cddd111016e21a25d255944aed5c0a
wjs_5025 <wjs_5025>
2023-08-01 15:04:24 +0900

    [FE] Chore : 더미데이터 변수명 수정

--- frontend/src/components/organisms/subscribe/SubscribeSlider/SubscribeSlider.tsx
+++ frontend/src/components/organisms/subscribe/SubscribeSlider/SubscribeSlider.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import dumySubscribe from 'dumy';
+import dummySubscribe from 'dummy';
 import SubscribeListItem from '../SubscribeListItem/SubscribeListItem';
 
 // 구독 목록 슬라이더
 function SubscribeSlider() {
 	return (
 		<Swiper>
-			{dumySubscribe.map((subscribe) => (
+			{dummySubscribe.map((subscribe) => (
 				<SwiperSlide key={subscribe.sid}>
 					<SubscribeListItem subscribe={subscribe} />
 				</SwiperSlide>
