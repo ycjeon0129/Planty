7c60e319b18cd0aea32d44b25f9d9ecd29affd09
GoldGyu <lkg2097@naver.com>
2023-08-01 10:48:10 +0900

    [FE] Chore : HomePage.tsx 초기화

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,42 +1,7 @@
 import React from 'react';
-import ToolBarLayout from 'components/layout/ToolBarLayout/ToolBarLayout';
-import ReservationTime from 'components/atoms/common/ReservationTime/ReservationTime';
 
 function HomePage() {
-	const Time = [
-		'10:00',
-		'10:30',
-		'11:00',
-		'11:30',
-		'12:00',
-		'12:30',
-		'13:00',
-		'13:30',
-		'14:00',
-		'14:30',
-		'15:00',
-		'15:30',
-		'16:00',
-		'16:30',
-		'17:00',
-		'17:30',
-	];
-	const reservationInfo: { isActivate: boolean } = {
-		isActivate: true, // true, false 커스터마이징
-	};
-
-	return (
-		<div>
-			<div>HomePage</div>
-			<div className="ReservationGrid">
-				{Time.map((time, index) => (
-					// eslint-disable-next-line react/no-array-index-key
-					<ReservationTime key={index} time={time} isActivate={reservationInfo.isActivate} />
-				))}
-			</div>
-			<ToolBarLayout />
-		</div>
-	);
+	return <div>HomePage</div>;
 }
 
 export default HomePage;
