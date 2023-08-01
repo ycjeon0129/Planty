6479cdbf9ccfaef4d0eb9d36d38acaae0a041ccd
전인혁 <wjs_5025@naver.com>
2023-08-02 00:33:11 +0900

    [FE] Feat : 로딩페이지 구현

--- frontend/src/pages/Etc/LoadingPage.tsx
+++ frontend/src/pages/Etc/LoadingPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Logo from 'assets/images/Logo.svg';
+import LoadingPageLayout from 'components/layout/Page/LoadingPageLayout/LoadingPageLayout';
 
 function LoadingPage() {
-	return <div />;
+	return (
+		<LoadingPageLayout>
+			<img src={Logo} alt="로딩. Planty Logo가 보여집니다." />
+		</LoadingPageLayout>
+	);
 }
 
 export default LoadingPage;
