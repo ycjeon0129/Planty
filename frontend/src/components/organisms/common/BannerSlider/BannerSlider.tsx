e06f64e2d8763505b9b0fbe6f2e4482f903ad8ce
wjs_5025 <wjs_5025>
2023-08-01 15:14:57 +0900

    [FE] Design : 배너 크기 설정 (width)

--- frontend/src/components/organisms/common/BannerSlider/BannerSlider.tsx
+++ frontend/src/components/organisms/common/BannerSlider/BannerSlider.tsx
@@ -1,10 +1,11 @@
 import { dummybanner } from 'dummy';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import './BannerSlider.scss';
 
 function BannerSlider() {
 	return (
-		<Swiper autoplay={{ delay: 2000, disableOnInteraction: false }}>
+		<Swiper className="banner-slider-container" autoplay={{ delay: 2000, disableOnInteraction: false }}>
 			{dummybanner.map((banner) => (
 				<SwiperSlide key={banner.id}>
 					<img src={banner.src} alt="" />
