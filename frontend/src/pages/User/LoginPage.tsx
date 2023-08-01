f14d6d47d2f273dc4a4cf306a49bcc9d6602c86a
전인혁 <wjs_5025@naver.com>
2023-08-02 01:23:35 +0900

    [FE] Feat : 로그인 페이지 구현

--- frontend/src/pages/User/LoginPage.tsx
+++ frontend/src/pages/User/LoginPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import LoginPageLayout from 'components/layout/Page/LoginPageLayout/LoginPageLayout';
 import Logo from 'assets/images/Logo.svg';
+import SocialLoginButton from 'components/atoms/user/GoogleLoginButton';
 
 function LoginPage() {
 	return (
 		<LoginPageLayout>
 			<img src={Logo} alt="" />
-			<button type="button">구글</button>
+			<SocialLoginButton />
 		</LoginPageLayout>
 	);
 }
