fc640b0623c39a4b142dd0d5377152259c18c50d
wjs_5025 <wjs_5025>
2023-08-01 14:51:05 +0900

    [FE] Feat : 구독 목록 슬라이더 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
 /* eslint-disable no-tabs */
 import AppRouter from 'AppRouter';
 import React from 'react';
+import SwiperCore, { Navigation } from 'swiper';
+
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+
+SwiperCore.use([Navigation]);
 
 function App() {
 	return <AppRouter />;
--- frontend/src/components/organisms/subscribe/SubscribeSlider/SubscribeSlider.tsx
+++ frontend/src/components/organisms/subscribe/SubscribeSlider/SubscribeSlider.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation } from 'swiper';
-
-import 'swiper/swiper.min.css';
-import 'swiper/components/navigation/navigation.min.css';
+import dumySubscribe from 'dumy';
 import SubscribeListItem from '../SubscribeListItem/SubscribeListItem';
 
 // 구독 목록 슬라이더
 function SubscribeSlider() {
-	// useSubscribe
-	SwiperCore.use([Navigation]);
-
 	return (
 		<Swiper>
-			<SwiperSlide>
-				<SubscribeListItem />
-			</SwiperSlide>
-			<SwiperSlide>
-				<SubscribeListItem />
-			</SwiperSlide>
-			<SwiperSlide>
-				<SubscribeListItem />
-			</SwiperSlide>
+			{dumySubscribe.map((subscribe) => (
+				<SwiperSlide key={subscribe.sid}>
+					<SubscribeListItem subscribe={subscribe} />
+				</SwiperSlide>
+			))}
 		</Swiper>
 	);
 }
