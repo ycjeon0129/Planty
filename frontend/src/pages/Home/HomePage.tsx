be72eb6d4a5946e5d575137025c91fc5f53870cb
wjs_5025 <wjs_5025>
2023-08-01 17:31:58 +0900

    [FE] Design : HomePage - 예약관리 컴포넌트 추가

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -3,6 +3,7 @@ import AreaTitle from 'components/atoms/common/AreaTitle/AreaTitle';
 import SubscribeSlider from 'components/organisms/subscribe/SubscribeSlider/SubscribeSlider';
 import React from 'react';
 import BannerSlider from 'components/organisms/common/BannerSlider/BannerSlider';
+import SquareShortcutButton from 'components/atoms/common/SquareShortcutButton/SquareShortcutButton';
 
 function HomePage() {
 	return (
@@ -10,7 +11,14 @@ function HomePage() {
 			<BannerSlider />
 			<AreaTitle title="내 구독 정보" url="subscribe" />
 			<SubscribeSlider />
-			<div>예약관리</div>
+			<AreaTitle title="서비스 바로가기" url="#" />
+			<SquareShortcutButton
+				message="예약관리"
+				onClick={() => {
+					alert('예약관리로 이동');
+				}}
+				type="booking"
+			/>
 			<div>채팅 응급실</div>
 			<div>화상 응급실</div>
 		</HomePageLayout>
