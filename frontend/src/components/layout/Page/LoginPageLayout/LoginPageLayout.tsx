c9b19e85c886270c0a35710c2832c89de70fe430
전인혁 <wjs_5025@naver.com>
2023-08-01 23:20:45 +0900

    [FE] Add : 페이지 파일 추가

---
+++ frontend/src/components/layout/Page/LoginPageLayout/LoginPageLayout.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+import './LoginPageLayout.scss';
+
+/**
+ * LoginPageLayout 구성하는 레이아웃
+ * @param children 레이아웃을 구성하는 컴포넌트들 ( Planty로고 / 소셜 로그인 버튼 )
+ */
+function LoginPageLayout({ children }: { children: ReactNode[] }) {
+	console.log(children);
+	return (
+		<div className="login-page-layout-container">
+			{/* 로고 */}
+			{children[0]}
+			{/* 소셜로그인 버튼 */}
+			{children[1]}
+		</div>
+	);
+}
+
+export default LoginPageLayout;
---
+++ frontend/src/pages/Etc/LoadingPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LoadingPage() {
+	return <div />;
+}
+
+export default LoadingPage;
