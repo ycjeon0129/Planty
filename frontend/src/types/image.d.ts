624fa927c6700b73d3d0789fb5466d6d5bbe28c3
전인혁 <wjs_5025@naver.com>
2023-08-03 01:31:49 +0900

    [FE] fix : 타입 수정

--- frontend/src/types/image.d.ts
+++ frontend/src/types/image.d.ts
@@ -2,7 +2,7 @@ declare module '*.svg' {
 	import React = require('react');
 
 	const src: string;
-	export const ReactComponent: REact.FC<React.SVGProps<SVGSVGElement>>;
+	export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
 	export default src;
 }
 
