2a44909472031f87027787651c3f2263bcf79a9d
임희선 <gmltjs4107@gmail.com>
2023-08-02 14:01:08 +0900

    [FE] Chore : Router 코드 구조 정리

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -28,26 +28,14 @@ function AppRouter() {
 				<BrowserRouter>
 					<Routes>
 						<Route path="/" element={<Navigate replace to="/home" />} />
-						<Route
-							path="/home"
-							element={
-								<PrivateRoute>
-									<HomePage />
-								</PrivateRoute>
-							}
-						/>
-						<Route
-							path="/mypage"
-							element={
-								<PrivateRoute>
-									<MypagePage />
-								</PrivateRoute>
-							}
-						/>
 						<Route path="/shop" element={<ShopPage />} />
 						<Route path="/emergency" element={<EmergencyPage />} />
-
 						<Route path="/login" element={<LoginPage />} />
+
+						<PrivateRoute>
+							<Route path="/home" element={<HomePage />} />
+							<Route path="/mypage" element={<MypagePage />} />
+						</PrivateRoute>
 
 						{/* 컴포넌트 개발용 */}
 						<Route path="/develop" element={<Develop />} />
