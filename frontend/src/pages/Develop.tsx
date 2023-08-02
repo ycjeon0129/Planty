5df1dc15b8622a51c52e8d3e045e3261e52666e4
wjs_5025 <wjs_5025>
2023-08-02 13:11:44 +0900

    [FE] Chore : 캘린더 테스트

--- frontend/src/pages/Develop.tsx
+++ frontend/src/pages/Develop.tsx
@@ -1,7 +1,13 @@
+import CustomCalendar from 'components/atoms/booking/CustomCalendar';
+import 'react-calendar/dist/Calendar.css';
 import React from 'react';
 
 function Develop() {
-	return <div />;
+	return (
+		<div>
+			<CustomCalendar />
+		</div>
+	);
 }
 
 export default Develop;
