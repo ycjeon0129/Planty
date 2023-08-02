0961bbdd978b7938e1f3825a6a2a62754904d67f
wjs_5025 <wjs_5025>
2023-08-02 12:03:49 +0900

    [FE] Design : 홈 구독 슬라이더 간격 조정

--- frontend/src/components/organisms/subscribe/SubscribeSlider/SubscribeSlider.tsx
+++ frontend/src/components/organisms/subscribe/SubscribeSlider/SubscribeSlider.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import dummySubscribe from 'dummy';
 import SubscribeListItem from '../SubscribeListItem/SubscribeListItem';
+import './SubscribeSlider.scss';
 
 /**
  * 구독 목록 슬라이더
  */
 function SubscribeSlider() {
 	return (
-		<Swiper>
+		<Swiper spaceBetween={10} slidesPerView="auto" className="subscribe-slider">
 			{dummySubscribe.map((subscribe) => (
 				<SwiperSlide key={subscribe.sid}>
 					<SubscribeListItem subscribe={subscribe} />
