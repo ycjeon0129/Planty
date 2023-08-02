f459f1721cf5d8b286b0d611681c13b86757a6a6
전인혁 <wjs_5025@naver.com>
2023-08-02 23:24:07 +0900

    [FE] Feat : 그린메이트 컴포넌트 배치

--- frontend/src/pages/Emergency/EmergencyPage.tsx
+++ frontend/src/pages/Emergency/EmergencyPage.tsx
@@ -4,6 +4,8 @@ import PageTitle from 'components/atoms/common/PageTitle/PageTitle';
 import Capsule from 'assets/icons/pageTitle/Capsule.svg';
 import AreaTitle from 'components/atoms/common/AreaTitle/AreaTitle';
 import SquareShortcutButton from 'components/atoms/common/SquareShortcutButton/SquareShortcutButton';
+import CurrentGreenmateCount from 'components/atoms/emergency/CurrentGreenmateCount/CurrentGreenmateCount';
+import TicketInfo from 'components/organisms/emergency/TicketInfo/TicketInfo';
 
 function EmergencyPage() {
 	return (
@@ -12,11 +14,11 @@ function EmergencyPage() {
 			<PageTitle icon={Capsule} text="응급실" />
 
 			{/* 현재 접속 중인 그린메이트 */}
-			<div>현재 접속 중인 그린메이트 15명</div>
+			<CurrentGreenmateCount />
 
 			{/* 이용권 정보 */}
 			<AreaTitle title="이용권 정보" url="#" />
-			<div>잔여 이용권 개수 / 이용권 구매하기</div>
+			<TicketInfo />
 
 			{/* 응급실 이용하기 */}
 			<AreaTitle title="응급실 이용하기" url="#" />
