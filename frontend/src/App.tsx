c444b2eeb37f71a88d0abefa4a7b44277beefe51
wjs_5025 <wjs_5025>
2023-08-02 13:54:32 +0900

    [FE] Add : Recoil 기본 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,11 +5,16 @@ import SwiperCore, { Navigation, Autoplay } from 'swiper';
 
 import 'swiper/swiper.min.css';
 import 'swiper/components/navigation/navigation.min.css';
+import { RecoilRoot } from 'recoil';
 
 SwiperCore.use([Navigation, Autoplay]);
 
 function App() {
-	return <AppRouter />;
+	return (
+		<RecoilRoot>
+			<AppRouter />
+		</RecoilRoot>
+	);
 }
 
 export default App;
---
+++ frontend/src/types/auth.ts
@@ -0,0 +1,8 @@
+export interface IUser {
+	nickname: string;
+	auth: string;
+	profilePhote: string;
+	joinDate: Date;
+	ticketCount: number;
+	address: string;
+}
