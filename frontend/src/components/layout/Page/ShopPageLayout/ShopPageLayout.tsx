2a2cab2304aefb0b9553f8a7ab29637f4225f752
임희선 <gmltjs4107@gmail.com>
2023-08-02 11:32:34 +0900

    [FE] Fix : 네비게이션 레이아웃에 컴포넌트 -> children[0]으로 변경

--- frontend/src/components/layout/Page/ShopPageLayout/ShopPageLayout.tsx
+++ frontend/src/components/layout/Page/ShopPageLayout/ShopPageLayout.tsx
@@ -3,23 +3,19 @@ import './ShopPageLayout.scss';
 import PageLayout from 'components/layout/common/PageLayout/PageLayout';
 import NavigationLayout from 'components/layout/navigation/NavigationLayout/NavigationLayout';
 import ContentsLayout from 'components/layout/common/ContentsLayout/ContentsLayout';
-import ShopIcon from 'assets/icons/pageTitle/ShoppingBag.svg';
-import PageTitle from 'components/atoms/common/PageTitle/PageTitle';
 
 function ShopPageLayout({ children }: { children: ReactNode[] }) {
 	return (
 		<PageLayout>
-			<NavigationLayout>
-				<PageTitle icon={ShopIcon} text="구독샵" />
-			</NavigationLayout>
+			<NavigationLayout>{children[0]}</NavigationLayout>
 
 			<div className="shop-page-layout-container">
 				{/* 구독 상품 목록(제목, 필터, 목록) */}
 				<ContentsLayout id="product-list-container">
-					{children[0]}
+					{children[1]}
 					<div id="product-list">
-						{children[1]}
 						{children[2]}
+						{children[3]}
 					</div>
 				</ContentsLayout>
 			</div>
