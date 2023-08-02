370cadab87666ea6645e801c7cfb8d6b96ac93c5
sion <sion941004@naver.com>
2023-08-02 16:12:04 +0900

    [FE] Design : FilterToggle 컴포넌트 구현

---
+++ frontend/src/components/atoms/common/FilterToggle/FilterToggle.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './FilterToggle.scss';
+import classnames from 'classnames';
+
+/**
+ * 원형의 채팅 버튼
+ * @param isActive 활성화상태[초록/회색]
+ * @param text 버튼내부 text
+ * @param handleClick 클릭함수
+ */
+function FilterToggle({ isActive, text, handleClick }: { isActive: boolean; text: string; handleClick: () => void }) {
+	const className = classnames('filter-button', { primary: isActive, disabled: !isActive });
+	return (
+		<button type="button" className={className} onClick={handleClick}>
+			{text}
+		</button>
+	);
+}
+
+export default FilterToggle;
