c31005c5e230c112cd1c5effe6f984b8ab1bb960
임희선 <gmltjs4107@gmail.com>
2023-08-02 11:32:56 +0900

    [FE] Design : 응급실 레이아웃 구현

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -5,6 +5,7 @@ import LoadingPage from 'pages/Etc/LoadingPage';
 import LoginPage from 'pages/User/LoginPage';
 import Develop from 'pages/Develop';
 import ShopPage from 'pages/Shop/ShopPage';
+import EmergencyPage from 'pages/Emergency/EmergencyPage';
 import PrivateRoute from './PrivateRoute';
 import 'styles/index.scss';
 
@@ -35,6 +36,8 @@ function AppRouter() {
 							}
 						/>
 						<Route path="/shop" element={<ShopPage />} />
+						<Route path="/emergency" element={<EmergencyPage />} />
+
 						<Route path="/login" element={<LoginPage />} />
 
 						{/* 컴포넌트 개발용 */}
