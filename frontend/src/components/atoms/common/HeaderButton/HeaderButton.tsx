bd3d4d6df04861e7e93217241c8cb74df21085eb
임희선 <gmltjs4107@gmail.com>
2023-08-03 11:38:06 +0900

    [FE] Chore : goBack 함수 추가, HeaderButton props icon -> type으로 수정

--- frontend/src/components/atoms/common/HeaderButton/HeaderButton.tsx
+++ frontend/src/components/atoms/common/HeaderButton/HeaderButton.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import './HeaderButton.scss';
+import BackIcon from 'assets/icons/Back.svg';
+import CloseIcon from 'assets/icons/Close.svg';
+import useMovePage from 'hooks/useMovePage';
 
 /**
  * 뒤로가기, 닫기 버튼이 있는 헤더 컴포넌트
  * @param icon string, 아이콘 url
  * @param text string, 버튼 옆에 있는 텍스트
  */
-function HeaderButton({ icon, text }: { icon: string; text: string }) {
+function HeaderButton({ type, text }: { type: string; text: string }) {
+	const { goBack } = useMovePage();
+
+	const icon = type === 'back' ? BackIcon : CloseIcon;
+	const handleClick = () => {
+		if (type === 'back') {
+			goBack();
+		} else {
+			// Close Page
+		}
+	};
+
 	return (
 		<div className="header-button">
-			<button
-				type="button"
-				onClick={() => {
-					alert('클릭');
-				}}
-			>
+			<button type="button" onClick={handleClick}>
 				<img src={icon} alt={text} height={14} width={14} />
 			</button>
 			<h1>{text}</h1>
