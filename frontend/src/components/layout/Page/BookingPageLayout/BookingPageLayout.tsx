1f16cbc5888891691bb1f1da38ca8793339935c7
전인혁 <wjs_5025>
2023-08-03 17:25:26 +0900

    [FE] Design : 예약페이지 레이아웃 수정

--- frontend/src/components/layout/Page/BookingPageLayout/BookingPageLayout.tsx
+++ frontend/src/components/layout/Page/BookingPageLayout/BookingPageLayout.tsx
@@ -2,14 +2,24 @@ import React, { ReactNode } from 'react';
 import './BookingPageLayout.scss';
 import PageLayout from 'components/layout/common/PageLayout/PageLayout';
 import NavigationLayout from 'components/layout/navigation/NavigationLayout/NavigationLayout';
+import ContentsLayout from 'components/layout/common/ContentsLayout/ContentsLayout';
 
 function BookingPageLayout({ children }: { children: ReactNode[] }) {
 	return (
 		<PageLayout>
+			{/* 페이지 헤더 */}
 			<NavigationLayout>{children[0]}</NavigationLayout>
-			<div className="booking-page-layout">{children[1]}</div>
-			<div>{children[2]}</div>
-			<div>{children[3]}</div>
+			<div className="booking-page-layout">
+				{/* 전체 예약 일정 */}
+				<ContentsLayout id="calendar">
+					{children[1]}
+					{children[2]}
+				</ContentsLayout>
+				<ContentsLayout id="booking-list">
+					{children[3]}
+					{children[4]}
+				</ContentsLayout>
+			</div>
 		</PageLayout>
 	);
 }
