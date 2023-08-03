ea49620d5713626869a47be3303fa8849ac577a7
전인혁 <wjs_5025>
2023-08-03 14:42:14 +0900

    [FE] Chore : 주석 변경

--- frontend/src/components/atoms/common/PageTitleButton/PageTitleButton.tsx
+++ frontend/src/components/atoms/common/PageTitleButton/PageTitleButton.tsx
@@ -6,7 +6,7 @@ import useMovePage from 'hooks/useMovePage';
 
 /**
  * 뒤로가기, 닫기 버튼이 있는 헤더 컴포넌트
- * @param icon string, 아이콘 url
+ * @param type string, back | close
  * @param text string, 버튼 옆에 있는 텍스트
  */
 function PageTitleButton({ type, text }: { type: string; text: string }) {
