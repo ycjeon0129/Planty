27fe92346e6c1d89bd232698d8df07a47db22c5e
GoldGyu <lkg2097@naver.com>
2023-08-03 17:52:11 +0900

    [FE] Add : ShopDetail 컴포넌트 교체

--- frontend/src/pages/Shop/ShopDetail.tsx
+++ frontend/src/pages/Shop/ShopDetail.tsx
@@ -1,7 +1,6 @@
-import PageTitle from 'components/atoms/common/PageTitle/PageTitle';
+import PageTitleButton from 'components/atoms/common/PageTitleButton/PageTitleButton';
 import ShopDetailPageLayout from 'components/layout/Page/ShopDetailPageLayout/ShopDetailPageLayout';
 import React from 'react';
-import BackIcon from 'assets/icons/Back.svg';
 import ProductIcon from 'assets/icons/product/product1.svg';
 import ProductIcon2 from 'assets/icons/product/product2.svg';
 import ProductImg from 'components/atoms/productdetail/productimg/ProductImg';
@@ -10,7 +9,7 @@ import Title from 'components/atoms/productdetail/title/Title';
 function ShopDetail() {
 	return (
 		<ShopDetailPageLayout>
-			<PageTitle icon={BackIcon} text="이전으로" />
+			<PageTitleButton type="back" text="이전으로" />
 			<ProductImg imgUrls={[ProductIcon, ProductIcon2]} />
 			<Title />
 			<div>4</div>
