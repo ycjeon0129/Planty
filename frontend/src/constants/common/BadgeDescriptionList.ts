f6dfd33bc73df34d3fc2988e13b346365945c912
임희선 <gmltjs4107@gmail.com>
2023-08-03 15:59:09 +0900

    [FE] Design : 뱃지 상태 설명 컴포넌트 구현

---
+++ frontend/src/constants/common/BadgeDescriptionList.ts
@@ -0,0 +1,7 @@
+const CONSULTING_STATUS_DESC_LIST = [
+	'※ 예약 취소는 예약 대기 1시간 이전까지 가능합니다.',
+	'※ 컨설팅 시작하기는 예약 시간 10분 전 활성화 됩니다.',
+	'※ 컨설팅 예약 후, 미참여 시에도 컨설팅 횟수가 차감됩니다.',
+];
+
+export default CONSULTING_STATUS_DESC_LIST;
