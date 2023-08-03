9a60821921ac5a639230f5bfa1ba71be20d1a8dd
전인혁 <wjs_5025>
2023-08-03 14:43:28 +0900

    [FE] Feat : 예약 캘린더 구현(30%)

--- frontend/src/components/atoms/booking/CustomCalendar/CustomCalendar.tsx
+++ frontend/src/components/atoms/booking/CustomCalendar/CustomCalendar.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import moment from 'moment';
 import Calendar from 'react-calendar';
+import './CustomCalendar.scss';
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];
@@ -8,8 +10,8 @@ function CustomCalendar() {
 	const [value, setValue] = useState<Value>(new Date());
 
 	return (
-		<div>
-			<Calendar onChange={setValue} value={value} />
+		<div className="custom-calendar-container">
+			<Calendar onChange={setValue} value={value} formatDay={(_, date) => moment(date).format('DD')} />
 			<span>{value?.toString()}</span>
 		</div>
 	);
