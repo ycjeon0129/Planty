dd56b40e44b4314cc60ea71b5c230fca25782a6d
전인혁 <wjs_5025>
2023-08-03 16:53:59 +0900

    [FE] Feat : 마이페이지 구현 (40%)

--- frontend/src/components/layout/Page/BookingPageLayout/BookingPageLayout.tsx
+++ frontend/src/components/layout/Page/BookingPageLayout/BookingPageLayout.tsx
@@ -7,8 +7,9 @@ function BookingPageLayout({ children }: { children: ReactNode[] }) {
 	return (
 		<PageLayout>
 			<NavigationLayout>{children[0]}</NavigationLayout>
-
 			<div className="booking-page-layout">{children[1]}</div>
+			<div>{children[2]}</div>
+			<div>{children[3]}</div>
 		</PageLayout>
 	);
 }
--- frontend/src/pages/BookingPage/BookingPage.tsx
+++ frontend/src/pages/BookingPage/BookingPage.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import AreaTitle from 'components/atoms/common/AreaTitle/AreaTitle';
 import BookingPageLayout from 'components/layout/Page/BookingPageLayout/BookingPageLayout';
 import PageTitleButton from 'components/atoms/common/PageTitleButton/PageTitleButton';
+import CustomCalendar from 'components/atoms/booking/CustomCalendar/CustomCalendar';
+import useSelectedDate from 'hooks/useSelectedDate';
+import ContentsLayout from 'components/layout/common/ContentsLayout/ContentsLayout';
 
 function BookingPage() {
+	const [date, setDate, formatDate] = useSelectedDate();
+
 	return (
 		<BookingPageLayout>
 			{/* 페이지 헤더 */}
@@ -11,6 +16,15 @@ function BookingPage() {
 
 			{/* 전체 예약 일정 */}
 			<AreaTitle title="전체 예약 일정" url="#" />
+
+			{/* 캘린더 */}
+			<ContentsLayout id="calendar">
+				<CustomCalendar selectedDate={date} setSelectedDate={setDate} />
+			</ContentsLayout>
+
+			{/* 지정일 예약목록 */}
+			<AreaTitle title={`${formatDate as string}  예약 목록`} url="#" />
+			<ContentsLayout id="booking-list"> gd</ContentsLayout>
 		</BookingPageLayout>
 	);
 }
