9a714921d25a87576435e75dd7c13215f8321491
임희선 <gmltjs4107@gmail.com>
2023-08-04 14:36:42 +0900

    [FE] Fix : props에 ConsultingHistory 타입 지정, div -> li

--- frontend/src/components/organisms/mypage/ConsultingDetail/ConsultingDetail.tsx
+++ frontend/src/components/organisms/mypage/ConsultingDetail/ConsultingDetail.tsx
@@ -1,32 +1,20 @@
 import React, { useState } from 'react';
-import './ConsultingDetails.scss';
+import './ConsultingDetail.scss';
 import { ReactComponent as OpenIcon } from 'assets/icons/arrow/DownBlack300.svg';
 import { ReactComponent as CloseIcon } from 'assets/icons/arrow/UpBlack300.svg';
 import SubscribeStateBadge from 'components/atoms/subscribe/SubscribeStateBadge/SubscribeStateBadge';
+import IConsultingHistory from 'types/consulting';
 
 /**
  * 컨설팅 내역 box
  * @param consultingDate	컨설팅일시
  * @param endDate 			컨설팅 종료일시
- * @param bookingState 		예약상황 (wait | done | end | join | notJoin)
+ * @param bookingState 		예약상황 (join | notJoin)
  * @param recommendDate		권장상담일
  * @param consultingComment 상담코멘트
  * @param isOpened 			상세정보 활성화상태
  */
-
-function ConsultingDetails({
-	consultingDate,
-	endDate,
-	bookingState,
-	recommendDate,
-	consultingComment,
-}: {
-	consultingDate: string;
-	endDate: string;
-	bookingState: string;
-	recommendDate: string;
-	consultingComment: string;
-}) {
+function ConsultingDetail({ data }: { data: IConsultingHistory }) {
 	const [isOpened, setIsOpened] = useState(false);
 
 	const toggleIsOpened = () => {
@@ -34,11 +22,11 @@ function ConsultingDetails({
 	};
 
 	return (
-		<div className="detail-container">
+		<li className="detail-container">
 			<div className="title">
-				<div className="flexgrow1">{consultingDate}</div>
+				<div className="flexgrow1">{data.consultingDate}</div>
 				<div className="flexgrow2">
-					<SubscribeStateBadge stateKey={bookingState} />
+					<SubscribeStateBadge stateKey={data.bookingState} />
 				</div>
 				<button type="button" className="flexgrow3" onClick={toggleIsOpened}>
 					{isOpened ? <CloseIcon /> : <OpenIcon />}
@@ -56,20 +44,20 @@ function ConsultingDetails({
 								<div className="mt-5">다음 권장 상담일</div>
 							</div>
 							<div className="rightdate">
-								<div>{consultingDate}</div>
-								<div className="mt-5">{endDate}</div>
-								<div className="mt-5">{recommendDate}</div>
+								<div>{data.consultingDate}</div>
+								<div className="mt-5">{data.endDate}</div>
+								<div className="mt-5">{data.recommendDate}</div>
 							</div>
 						</div>
 					</div>
 					<div className="content-box">
 						<div className="h3">상담내용</div>
-						<div className="content">{consultingComment}</div>
+						<div className="content">{data.consultingComment}</div>
 					</div>
 				</div>
 			)}
-		</div>
+		</li>
 	);
 }
 
-export default ConsultingDetails;
+export default ConsultingDetail;
