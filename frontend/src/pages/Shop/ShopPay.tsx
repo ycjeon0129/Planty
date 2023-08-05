d506bbfbe7f43b71bc0edc513d5120da0cc25307
Gold-gyu <lkg2097@naver.com>
2023-08-05 17:09:16 +0900

    [FE] Chore : 결제페이지 props 정리

--- frontend/src/pages/Shop/ShopPay.tsx
+++ frontend/src/pages/Shop/ShopPay.tsx
@@ -7,7 +7,7 @@ function ShopPay() {
 			<div> 이전으로 </div>
 			<div> 주문/결제 </div>
 			<div> 주문자 정보 </div>
-			<div> 배송지 주소 (필수) </div>
+			<div> 배송지 주소(필수) </div>
 			<div> 주문 상품 정보 </div>
 			<div> 결제금액확인 </div>
 			<div> 구매하기 </div>
