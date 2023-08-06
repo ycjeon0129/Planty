d71caedfae50470a0c5580527f4db35824f19c35
전인혁 <wjs_5025@naver.com>
2023-08-06 20:58:32 +0900

    [FE] Add : 차트 관련 타입 추가

--- frontend/src/types/global.d.ts
+++ frontend/src/types/global.d.ts
@@ -1,3 +1,25 @@
 // 캘린더의 선택된 날짜에 대한 타입
 export type ValuePiece = Date | null;
 export type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+// 차트 관련
+export interface IPlantDatasets {
+	label: string;
+	fill: boolean;
+	tension: number;
+	data: Array;
+	borderColor: string;
+	pointRadius: number;
+}
+
+export interface IPlantData {
+	date: string;
+	temperature: number;
+	humidity: number;
+	soilHumidity: number;
+}
+
+export interface IChartData {
+	labels: string[];
+	datasets: Chart.ChartDataset<'line', number[], string>[];
+}
