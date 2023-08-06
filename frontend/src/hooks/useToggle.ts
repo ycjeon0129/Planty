368bda7edf933693a96f70369c9089c3649b621d
전인혁 <wjs_5025@naver.com>
2023-08-06 20:56:38 +0900

    [FE] Feat : 온/습도 관련 커스텀 훅 구현

---
+++ frontend/src/hooks/useToggle.ts
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+/**
+ * 토글버튼 상태 변수와 토글 함수를 생성하고, 이를 반환하는 커스텀 훅
+ * @returns state : 토글버튼 상태 변수
+ * @returns toggle : 토글함수 (토글 상태변수를 반대로 바꿔줌.)
+ */
+const useToggle = (): [boolean, () => void] => {
+	const [state, setState] = useState(false);
+
+	const toggle = () => {
+		setState(!state);
+	};
+
+	return [state, toggle];
+};
+
+export default useToggle;
