96b84a7c617de14eced6b20a78f4c1a7f2ee941b
전인혁 <wjs_5025@naver.com>
2023-08-07 03:44:53 +0900

    [FE] Chore : import 방식 수정

--- frontend/src/types/consulting.d.ts
+++ frontend/src/types/consulting.d.ts
@@ -1,4 +1,4 @@
-interface IConsultingHistory {
+export interface IConsultingHistory {
 	consultingDate: string;
 	endDate: string;
 	bookingState: string;
@@ -6,4 +6,16 @@ interface IConsultingHistory {
 	consultingComment: string;
 }
 
-export default IConsultingHistory;
+export type ConsultingSession = Session | null;
+export type ConsultingSubscribers = ISubscriber[];
+export type ConsultingDevice = Device | null;
+export type ConsultingPublisher = Publisher | null;
+export type ConsultingStreamManager = StreamManager | null;
+
+export type SetConsultingSessionFn = (session: Session) => void;
+export type SetonsultingSubscribersFn = (subscribers: ISubscriber[]) => void;
+export type SetonsultingDeviceFn = (device: Device) => void;
+export type SetonsultingPublisherFn = (publisher: Publisher) => void;
+export type SetonsultingMainStreamManagerFn = (streamManager: StreamManager) => void;
+
+export interface ISubscriber {}
