31e1592ea39f54be77f698ae969060d2df3f0f09
전인혁 <wjs_5025@naver.com>
2023-08-08 03:41:27 +0900

    [FE/GM] Feat : 에러페이지 구현

---
+++ greenmate/planty-greenmate/src/pages/etc/ErrorPage.tsx
@@ -0,0 +1,16 @@
+import ErrorPageLayout from 'components/layout/page/ErrorPageLayout/ErrorPageLayout';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Plant } from 'assets/images/SadPlant.svg';
+
+function ErrorPage() {
+	return (
+		<ErrorPageLayout>
+			<Plant />
+			<h3>잘못된 페이지 요청입니다</h3>
+			<Link to="/home">홈으로 돌아가기</Link>
+		</ErrorPageLayout>
+	);
+}
+
+export default ErrorPage;
