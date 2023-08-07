d468e66ba9bd9e2ae183900dd471a77224375445
sion <sion941004@naver.com>
2023-08-07 16:40:45 +0900

    [FE] Chore : 응급실 참여 페이지 Router 추가

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -19,6 +19,7 @@ import BookingPage from 'pages/subscribe/Booking/BookingPage';
 import ConsultingHistoryPage from 'pages/subscribe/ConsultingHistory/ConsultingHistoryPage';
 import ShopPay from 'pages/Shop/ShopPay';
 import ConsultingLoadingPageLayout from 'components/layout/Page/ConsultingLoadingPageLayout/ConsultingLoadingPageLayout';
+import EmergencyParticipatePage from 'pages/Emergency/EmergencyParticipatePage';
 import PrivateRoute from './PrivateRoute';
 import 'styles/index.scss';
 
@@ -50,6 +51,7 @@ function AppRouter() {
 						<Route path="/shop/detail/:pid" element={<ShopDetail />} />
 						<Route path="/shop/pay/:pid" element={<ShopPay />} />
 						<Route path="/emergency" element={<EmergencyPage />} />
+						<Route path="/emergency/participate" element={<EmergencyParticipatePage />} />
 						<Route path="/mypage" element={<MypagePage />} />
 						<Route path="/mypage/booking" element={<BookingManagementPage />} />
 						<Route path="/mypage/:menu" element={<MypageSubMenuDetailPage />} />
