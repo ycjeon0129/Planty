234dcc2c8879b92ba11e8f6607a7b1dce28f1555
GoldGyu <lkg2097@naver.com>
2023-08-07 17:57:57 +0900

    [FE] Add  : 그린메이트페이지 로그인 로고 컴포넌트 추가

--- greenmate/planty-greenmate/src/pages/login/LoginPage.tsx
+++ greenmate/planty-greenmate/src/pages/login/LoginPage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import LoginPageLayout from 'components/layout/page/loginpagelayout/LoginPageLayout';
+import LoginLogo from 'components/atoms/loginlogo/LoginLogo';
 
 function LoginPage() {
 	return (
 		<LoginPageLayout>
-			<div>로고</div>
+			<LoginLogo />
 			<div>로그인</div>
 			<div>로그인버튼</div>
 		</LoginPageLayout>
