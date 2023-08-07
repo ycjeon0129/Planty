82608d196c53df74612ada86c306e5d2153272e6
sion <sion941004@naver.com>
2023-08-07 14:25:30 +0900

    [FE] Chore TicketRemains 가운데정렬

--- frontend/src/components/organisms/emergency/TicketRemains/TicketRemains.tsx
+++ frontend/src/components/organisms/emergency/TicketRemains/TicketRemains.tsx
@@ -10,12 +10,14 @@ import useUser from 'hooks/useUser';
 function TicketRemains() {
 	const [user] = useUser();
 	return (
-		<div className="ticketremains-container">
-			<div>Planty 응급실 서비스</div>
-			<div className="ticketset">
-				<TicketSet />
+		<div className="center">
+			<div className="ticketremains-container">
+				<div>Planty 응급실 서비스</div>
+				<div className="ticketset">
+					<TicketSet />
+				</div>
+				남은 이용권 갯수 : {user?.ticketCount}
 			</div>
-			남은 이용권 갯수 : {user?.ticketCount}
 		</div>
 	);
 }
