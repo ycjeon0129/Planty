363b352ba15b40af54c1c7576cc83eaa4d4c3471
전인혁 <wjs_5025@naver.com>
2023-08-08 22:27:51 +0900

    [FE] Feat : Response를 객체로 만드는 유틸함수 구현

---
+++ frontend/src/utils/subscribes/responseToSubscribe.ts
@@ -0,0 +1,30 @@
+import { ISubscribe, ISubscribeResponse } from 'types/subscribe';
+import getSubscribeState from './getSubscribeState';
+
+/**
+ * 실행여부, 취소여부, 종료여부, 컨설팅 일자로 구독의 현재 상태를 리턴
+ * @param active 실행여부
+ * @param cancel 취소여부
+ * @param end 종료여부
+ * @param consultingDate 가까운 컨설팅 일자
+ * @returns 'wait' | 'done' | 'end' | 'join' | 'notJoin'
+ */
+const responseToSubscribe = (res: ISubscribeResponse): ISubscribe => {
+	return {
+		sid: res.sid,
+		startDate: res.startDate,
+		title: res.title,
+		info: {
+			consultingCnt: res.consultingCnt,
+			consultingRemainCnt: res.consultingRemainCnt,
+			consultingDate: res.consultingDate,
+			consultingCancel: res.consultingCancel,
+			consultingActive: res.consultingActive,
+			consultingTime: res.consultingTime,
+		},
+		state: getSubscribeState(res.consultingActive, res.consultingCancel, res.end, res.consultingDate),
+		thumbnail: 'https://gardening.godohosting.com/2018/mfset/redcherrytomato_01.jpg',
+	};
+};
+
+export default responseToSubscribe;
