44a957490731bf1b237143d20fbc42445762ebe7
전인혁 <wjs_5025@naver.com>
2023-08-09 04:44:38 +0900

    [FE/GM] Add : 캘린더 컴포넌트 가져오기

--- greenmate/planty-greenmate/src/pages/Subscribes/CalendarPage.tsx
+++ greenmate/planty-greenmate/src/pages/Subscribes/CalendarPage.tsx
@@ -1,8 +1,21 @@
 import CalendarPageLayout from 'components/layout/page/Subscribes/CalendarPageLayout/CalendarPageLayout';
+import BookingList from 'components/organisms/subscribes/BookingList/BookingList';
+import FullScheduleCalendar from 'components/organisms/subscribes/FullScheduleCalendar/FullScheduleCalendar';
+import useSelectedDate from 'hooks/useSelectedDate';
 import React from 'react';
 
 function CalendarPage() {
-	return <CalendarPageLayout>캘린더페이지</CalendarPageLayout>;
+	const [date, setDate, formatDate] = useSelectedDate();
+
+	return (
+		<CalendarPageLayout>
+			<FullScheduleCalendar selectedDate={date} setSelectedDate={setDate} />
+			<div>
+				<h3>{formatDate}</h3>
+				<BookingList />
+			</div>
+		</CalendarPageLayout>
+	);
 }
 
 export default CalendarPage;
