e9563dc7d51720bc4c90305b1d75c61e21e8b620
전인혁 <wjs_5025>
2023-08-08 11:37:49 +0900

    [FE/GM] Design : 페이지 레이아웃 수정

--- greenmate/planty-greenmate/src/components/layout/page/Consulting/VideoPageLayout/VideoPageLayout.tsx
+++ greenmate/planty-greenmate/src/components/layout/page/Consulting/VideoPageLayout/VideoPageLayout.tsx
@@ -1,19 +1,15 @@
 import React, { ReactNode } from 'react';
 import './VideoPageLayout.scss';
 import PageLayout from 'components/layout/common/PageLayout/PageLayout';
-import ContentsLayout from 'components/layout/common/ContentsLayout/ContentsLayout';
 
 function VideoPageLayout({ children }: { children: ReactNode[] }) {
 	return (
 		<PageLayout isFullPage>
-			<div className="video-page-layout-container">
-				<div id="left">
-					<ContentsLayout id="now-consulting-area">{children[0]}</ContentsLayout>
-					<ContentsLayout id="my-subscribe-area">{children[1]}</ContentsLayout>
-				</div>
-				<div id="right">
-					<ContentsLayout id="soon-booking-area">{children[2]}</ContentsLayout>
-				</div>
+			<div id="header">{children[0]}</div>
+			<div id="video-page-layout-container">
+				<div id="user-video">{children[1]}</div>
+				<div id="greenmate-video">{children[2]}</div>
+				<div id="video-consulting-menu">{children[3]}</div>
 			</div>
 		</PageLayout>
 	);
