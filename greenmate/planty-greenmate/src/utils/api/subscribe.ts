93bd4f8169323e8ed40f4675a5e33c45d2ad6a95
전인혁 <wjs_5025@naver.com>
2023-08-09 03:18:34 +0900

    [FE/GM] Feat : 구독 관련 api 구현

--- greenmate/planty-greenmate/src/utils/api/subscribe.ts
+++ greenmate/planty-greenmate/src/utils/api/subscribe.ts
@@ -1,19 +1,19 @@
 import { instance } from './instance';
 
-/**
- * 식물의 온/습도 데이터를 요청
- * @url /greenmate/connection
- * @returns response
- */
-
-export const findPlantDataApi = async (sid: number) => {
-	const response = await instance.get(`/subscribe/${sid}/plants`);
+export const findAllSubscribeApi = async () => {
+	const response = await instance.get('/subscribes');
 
 	return response;
 };
 
-export const findSubscribes = async () => {
-	const response = await instance.get('/greenmate/connection');
+export const findSubscribeApi = async (sid: number) => {
+	const response = await instance.get(`/subscribe/${sid}`);
+
+	return response;
+};
+
+export const findAllBookingApi = async () => {
+	const response = await instance.get('/bookings');
 
 	return response;
 };
