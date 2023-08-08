2458ee91d65a2c329bfc8fabcc5fbdf593130495
전인혁 <wjs_5025@naver.com>
2023-08-09 04:43:06 +0900

    [FE/GM] Feat : 구독관리 관련 컴포넌트 구현

--- greenmate/planty-greenmate/src/components/organisms/subscribes/BookingList/BookingList.tsx
+++ greenmate/planty-greenmate/src/components/organisms/subscribes/BookingList/BookingList.tsx
@@ -1,13 +1,23 @@
+/* eslint-disable react/require-default-props */
 import React from 'react';
 import BookingListItem from 'components/atoms/subscribes/BookingListItem/BookingListItem';
 import useAllBooking from 'hooks/api/useAllBooking';
 import './BookingList.scss';
+import { Value } from 'types/global';
+import useBookingList from 'hooks/api/useBookingList';
 
-function BookingList() {
+function BookingList({ date }: { date?: Value }) {
+	const bookingsInDate = useBookingList(date as Value);
 	const bookings = useAllBooking();
 
-	if (bookings.length)
-		return <div className="booking-list-container">{bookings?.map((b) => <BookingListItem booking={b} />)}</div>;
+	if (date ? bookingsInDate.length : bookings.length)
+		return (
+			<div className="booking-list-container">
+				{date
+					? bookingsInDate?.map((b) => <BookingListItem booking={b} />)
+					: bookings?.map((b) => <BookingListItem booking={b} />)}
+			</div>
+		);
 
 	return (
 		<div className="booking-list-container no-content">
