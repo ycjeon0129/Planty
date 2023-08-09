40a0271b10239fea7ccd912cbe4edb3f14bca542
전인혁 <wjs_5025@naver.com>
2023-08-10 03:35:58 +0900

    [FE/GM] Design : css 속성 추가

--- greenmate/planty-greenmate/src/components/organisms/history/EmergencyDetail/EmergencyDetail.tsx
+++ greenmate/planty-greenmate/src/components/organisms/history/EmergencyDetail/EmergencyDetail.tsx
@@ -3,6 +3,7 @@ import InfoRow from 'components/atoms/common/InfoRow/InfoRow';
 import React from 'react';
 import converTime from 'utils/converTime';
 import formatDate from 'utils/formatDate';
+import './EmergencyDetail.scss';
 // import useLocationIdx from 'hooks/common/useSid';
 // import useEmergency from 'hooks/api/useEmergency';
 
