6559ee1bb7fbb01b277b985ce001991a980656bf
GoldGyu <lkg2097@naver.com>
2023-08-09 10:37:06 +0900

    [FE] Feat : 대시보드 컴포넌트 제작

---
+++ greenmate/planty-greenmate/src/components/atoms/currentProgress/CurrentProgress.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './CurrentProgress.scss';
+import GreenmateImg from 'assets/icons/Greenmate.svg';
+import ProgressImg from 'assets//icons/CurrentProgress.svg';
+import Title from '../title/Title';
+
+// 받아야하는 인자
+// 컨설턴트 사진, 컨설턴트 이름, 서비스 유형, 컨설팅 방식, 시작 시간
+function CurrentProgress() {
+	return (
+		<div className="current-progress-outer-box">
+			<div className="current-progress-top-box">
+				<Title title="현재 진행 중인 컨설팅" />
+			</div>
+			<div className="current-progress-button-box">
+				<div className="current-progress-bt-box1">
+					<div className="current-progress-left-box">
+						<br />
+						<div className="greenmate-img-box">
+							<img src={GreenmateImg} alt="사진" />
+						</div>
+						<Title title="컨설턴트 이름" />
+					</div>
+					<div className="current-progress-text-box">
+						<div className="current-progress-right-box">
+							<div className="gray-text"> 서비스 유형</div>
+							<div>응급실</div>
+						</div>
+						<div className="current-progress-right-box">
+							<div className="gray-text">컨설팅 방식</div>
+							<div>응급실</div>
+						</div>
+						<div className="current-progress-right-box">
+							<div className="gray-text">시작시간</div>
+							<div>응급실</div>
+						</div>
+					</div>
+				</div>
+				<div className="current-progress-bt-right-box">
+					<div>
+						<img src={ProgressImg} alt="" />
+					</div>
+					<div className="primary-text">24분째 진행중</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default CurrentProgress;
--- greenmate/planty-greenmate/src/pages/DashBoard/DashBoardPage.tsx
+++ greenmate/planty-greenmate/src/pages/DashBoard/DashBoardPage.tsx
@@ -6,13 +6,14 @@ import Img from 'assets/icons/product/product1.svg';
 import Title from 'components/atoms/title/Title';
 import ClassReservation from 'components/atoms/classreservation/ClassReservation';
 import Img2 from 'assets/icons/GreenP.svg';
+import CurrentProgress from 'components/atoms/currentProgress/CurrentProgress';
 
 function DashBoardPage() {
 	// 내 구독 정보(개수, 상품) 가져와서 보여주기
 	return (
 		<DashBoardPageLayout>
 			<PageHeader text="대시보드" />
-			<div>1</div>
+			<CurrentProgress />
 			<Title title="내 구독" />
 			<ClassInfo img={Img} title="4주만에 끝내는 토마토 클래스" date="2023/07/03(월) ~ 2023/07/28(금)" />
 			<Title title="다가오는 컨설팅 일정" />
