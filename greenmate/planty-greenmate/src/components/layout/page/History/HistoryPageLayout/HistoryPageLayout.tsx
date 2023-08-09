657e7a4331dff20563643f3c1a4da992cf3b93c9
전인혁 <wjs_5025@naver.com>
2023-08-10 03:34:28 +0900

    [FE/GM] Design : 페이지 레이아웃 지정

--- greenmate/planty-greenmate/src/components/layout/page/History/HistoryPageLayout/HistoryPageLayout.tsx
+++ greenmate/planty-greenmate/src/components/layout/page/History/HistoryPageLayout/HistoryPageLayout.tsx
@@ -1,12 +1,15 @@
 import React, { ReactNode } from 'react';
 import PageLayout from 'components/layout/common/PageLayout/PageLayout';
+import './HistoryPageLayout.scss';
 
 function HistoryPageLayout({ children }: { children: ReactNode[] }) {
 	return (
 		<PageLayout>
-			{children[0]}
-			{children[1]}
-			{children[2]}
+			<div className="history-page-layout-container">
+				{children[0]}
+				{children[1]}
+				{children[2]}
+			</div>
 		</PageLayout>
 	);
 }
