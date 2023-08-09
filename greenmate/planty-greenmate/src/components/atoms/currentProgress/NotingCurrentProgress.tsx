4152422cb6f3e4436fd9c3304a30fe38de616bf5
GoldGyu <lkg2097@naver.com>
2023-08-09 11:11:38 +0900

    [FE] Feat : 대시보드 수정

---
+++ greenmate/planty-greenmate/src/components/atoms/currentProgress/NotingCurrentProgress.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NotingCurrentProgress.scss';
+
+function NotingCurrentProgress() {
+	return (
+		<div className="noting-progress-box">
+			<div className="noting-gray-text">현재 진행중인 컨설팅이 없습니다.</div>
+		</div>
+	);
+}
+
+export default NotingCurrentProgress;
--- greenmate/planty-greenmate/src/pages/DashBoard/DashBoardPage.tsx
+++ greenmate/planty-greenmate/src/pages/DashBoard/DashBoardPage.tsx
@@ -7,13 +7,16 @@ import Title from 'components/atoms/title/Title';
 import ClassReservation from 'components/atoms/classreservation/ClassReservation';
 import Img2 from 'assets/icons/GreenP.svg';
 import CurrentProgress from 'components/atoms/currentProgress/CurrentProgress';
+import NotingCurrentProgress from 'components/atoms/currentProgress/NotingCurrentProgress';
 
 function DashBoardPage() {
 	// 내 구독 정보(개수, 상품) 가져와서 보여주기
+	// 현재 진행중인 컨설팅이 있는지 확인하는 상수 필요
+	const DoingConsult = 1;
 	return (
 		<DashBoardPageLayout>
 			<PageHeader text="대시보드" />
-			<CurrentProgress />
+			{DoingConsult ? <CurrentProgress /> : <NotingCurrentProgress />}
 			<Title title="내 구독" />
 			<ClassInfo img={Img} title="4주만에 끝내는 토마토 클래스" date="2023/07/03(월) ~ 2023/07/28(금)" />
 			<Title title="다가오는 컨설팅 일정" />
