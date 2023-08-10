781a8138bdb51f92ba33bc487357ce4b435163d1
전인혁 <wjs_5025>
2023-08-10 17:11:13 +0900

    [FE/GM] Feat : 예약 리스트 구현

--- greenmate/planty-greenmate/src/components/organisms/subscribes/BookingList/BookingList.tsx
+++ greenmate/planty-greenmate/src/components/organisms/subscribes/BookingList/BookingList.tsx
@@ -1,23 +1,14 @@
 /* eslint-disable react/require-default-props */
 import React from 'react';
 import BookingListItem from 'components/atoms/subscribes/BookingListItem/BookingListItem';
-import useAllBooking from 'hooks/api/useAllBooking';
 import './BookingList.scss';
-import { Value } from 'types/base/global';
-import useBookingList from 'hooks/api/useBookingList';
+import useAllBooking from 'hooks/api/useAllBooking';
 
-function BookingList({ date }: { date?: Value }) {
-	const bookingsInDate = useBookingList(date as Value);
+function BookingList() {
 	const bookings = useAllBooking();
 
-	if (date ? bookingsInDate.length : bookings.length)
-		return (
-			<div className="booking-list-container">
-				{date
-					? bookingsInDate?.map((b) => <BookingListItem booking={b} />)
-					: bookings?.map((b) => <BookingListItem booking={b} />)}
-			</div>
-		);
+	if (bookings.length)
+		return <div className="booking-list-container">{bookings?.map((b) => <BookingListItem booking={b} />)}</div>;
 
 	return (
 		<div className="booking-list-container no-content">
