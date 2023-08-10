48fd84f05d87019c58d200e98dfeb01d7bd09405
전인혁 <wjs_5025@naver.com>
2023-08-10 22:19:32 +0900

    [FE/GM] Feat : 요청 리스트 구현

--- greenmate/planty-greenmate/src/components/atoms/sidebar/requestItem/RequestItem.tsx
+++ greenmate/planty-greenmate/src/components/atoms/sidebar/requestItem/RequestItem.tsx
@@ -2,26 +2,20 @@ import React from 'react';
 import './RequestItem.scss';
 import x from 'assets/icons/Close.svg';
 
-interface RequestProps {
-	type: string;
-	greenmate: string;
-	service: string;
-}
-
 /**
  * 사용자가 요청보낸 것(*수락버튼 + x버튼 눌렀을 떄 로직 미완*)
  * @param param0 Type : 채팅 or 화상, greenmate: 그린메이트 닉네임, service: 응급실, 정기 컨설팅
  * @returns
  */
-function RequestItem({ type, greenmate, service }: RequestProps) {
+function RequestItem({ type, user, category }: { type: number; user: string; category: number }) {
 	return (
 		<div>
 			<div className="request-outer-box">
 				<div className="request-left-box">
-					<div className="bold-text">{type}</div>
+					<div className="bold-text">{type ? '화상' : '구독'}</div>
 					<div>
-						<div>{greenmate}님</div>
-						<div>{service} 요청</div>
+						<div>{user}님</div>
+						<div>{category ? '구독 컨설팅' : '응급실 컨설팅'} 요청</div>
 					</div>
 				</div>
 				<div>
--- greenmate/planty-greenmate/src/components/organisms/common/RequestList/RequestList.tsx
+++ greenmate/planty-greenmate/src/components/organisms/common/RequestList/RequestList.tsx
@@ -1,16 +1,21 @@
 import RequestItem from 'components/atoms/sidebar/requestItem/RequestItem';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './RequestList.scss';
 import { findAllRequest } from 'utils/api/consulting';
+import { IConsultingRequest } from 'types/consulting';
+import { useRecoilState } from 'recoil';
+import authState from 'recoil/auth';
 
 function RequestList() {
-	// const [requests, setRequests] = useState();
+	const [requests, setRequests] = useState<IConsultingRequest[]>([]);
+	const [auth] = useRecoilState(authState);
+	const [msg, setMsg] = useState('요청을 수신하려면 활동상태를 활성화하세요.');
 
 	const fetchData = async () => {
 		try {
 			const response = await findAllRequest();
 			console.log(response);
-			// setRequests();
+			setRequests(response.data);
 		} catch (error) {
 			console.error(error);
 		}
@@ -20,9 +25,20 @@ function RequestList() {
 		fetchData();
 	}, []);
 
+	useEffect(() => {
+		if (auth.status) setMsg('수신된 컨설팅 요청이 없습니다.');
+		else setMsg('요청을 수신하려면\n활동상태를 활성화하세요.');
+	}, [auth.status]);
+
 	return (
 		<div className="request-list-container">
-			<RequestItem type="채팅" greenmate="영국여자" service="응급실" />
+			{requests.length ? (
+				requests.map((r) => <RequestItem type={r.type} user={r.user} category={r.category} />)
+			) : (
+				<div id="no-content" className={auth.status ? '' : 'status-off'}>
+					{msg}
+				</div>
+			)}
 		</div>
 	);
 }
