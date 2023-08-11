f10d977d10daf490081d091c34fbb510376943d5
전인혁 <wjs_5025@naver.com>
2023-08-11 21:03:05 +0900

    [FE] Feat : 삭제 api 구현

--- frontend/src/components/organisms/booking/BookingListItem/BookingListItem.tsx
+++ frontend/src/components/organisms/booking/BookingListItem/BookingListItem.tsx
@@ -8,8 +8,24 @@ import 'moment/locale/ko';
 import { IBooking } from 'types/domain/booking';
 import convertTime from 'utils/common/convertTime';
 import InfoRow from 'components/atoms/common/InfoRow/InfoRow';
+import confirmDialog from 'utils/common/confirmDialog';
+import { deleteBooking } from 'utils/api/booking';
 
 function BookingListItem({ booking }: { booking: IBooking }) {
+	const dialogMessage = `${booking.date} ${convertTime(booking.time)}에 진행되는 '${
+		booking.title
+	}'의 예약을 정말 취소하시겠습니까?`;
+
+	async function cancelBooking() {
+		try {
+			const response = await deleteBooking(booking.cid);
+			// TODO : 삭제 후 리로드
+			console.log(response);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
 	return (
 		<Accordion className="booking-list-item-container">
 			<AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
@@ -23,7 +39,12 @@ function BookingListItem({ booking }: { booking: IBooking }) {
 			<AccordionDetails>
 				<div className="booking-info">
 					<div id="detail">
-						<h3>예약 정보 상세</h3>
+						<div id="detail-header">
+							<h3>예약 정보 상세</h3>
+							<button type="button" onClick={() => confirmDialog(dialogMessage, cancelBooking)}>
+								예약 취소
+							</button>
+						</div>
 						<InfoRow title="- 가드너" content={booking.user} />
 						<InfoRow title="- 그린메이트" content={booking.greenmate} />
 					</div>
--- frontend/src/utils/api/booking.ts
+++ frontend/src/utils/api/booking.ts
@@ -4,9 +4,13 @@ import { instance } from './instance';
  * 예약(컨설팅) 목록 전체조회 요청
  * @param date optional. Date 객체가 들어오면 해당 날짜에 해당하는 예약 목록만 출력.
  */
-const findAllBooking = async () => {
+export const findAllBooking = async () => {
 	const response = await instance.get(`/bookings`);
 	return response;
 };
 
-export default findAllBooking;
+export const deleteBooking = async (cid: number) => {
+	const response = await instance.delete(`/bookings/${cid}`);
+
+	return response;
+};
