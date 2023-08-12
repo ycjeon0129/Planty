15c44ae9073c6b20c3af597dea449052fc297dd7
전윤철 <tlfldntm4444@naver.com>
2023-08-13 01:43:03 +0900

    [BE] Feat : 그린메이트 예약 전체 조회 - 특정 구독만 조회하기 추가

--- backend/planty-service/src/main/java/com/planty/api/gm/booking/controller/GmBookingController.java
+++ backend/planty-service/src/main/java/com/planty/api/gm/booking/controller/GmBookingController.java
@@ -2,13 +2,16 @@ package com.planty.api.gm.booking.controller;
 
 import com.planty.api.booking.response.BookingResponse;
 import com.planty.api.booking.service.BookingService;
+import com.planty.api.gm.booking.response.GmBookingResponse;
 import com.planty.api.gm.booking.service.GmBookingService;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,13 +28,16 @@ public class GmBookingController {
     private final GmBookingService gmBookingServiceImpl;
 
     @GetMapping // 그린메이트 예약 조회
-    public ResponseEntity<?> getUserBooking() {
+    public ResponseEntity<List<GmBookingResponse>> getUserBooking(
+            @RequestParam(value = "spid", required = false) Long spid
+    ) {
         log.info(logCurrent(getClassName(), getMethodName(), START));
-        List<BookingResponse> bookingList = gmBookingServiceImpl.getGmBooking();
+        List<GmBookingResponse> bookingList = gmBookingServiceImpl.getGmBooking(spid);
 
         if (!bookingList.isEmpty()) {
             log.info(logCurrent(getClassName(), getMethodName(), END));
-            return ResponseEntity.status(200).body(bookingList);
+//            return ResponseEntity.status(200).body(bookingList);
+            return new ResponseEntity<List<GmBookingResponse>>(bookingList, HttpStatus.OK);
         }
         log.info(logCurrent(getClassName(), getMethodName(), END));
         return ResponseEntity.status(204).build();
--- backend/planty-service/src/main/java/com/planty/api/gm/booking/service/GmBookingServiceImpl.java
+++ backend/planty-service/src/main/java/com/planty/api/gm/booking/service/GmBookingServiceImpl.java
@@ -1,6 +1,7 @@
 package com.planty.api.gm.booking.service;
 
 import com.planty.api.booking.response.BookingResponse;
+import com.planty.api.gm.booking.response.GmBookingResponse;
 import com.planty.common.exception.handler.ExceptionHandler;
 import com.planty.common.util.SecurityUtil;
 import com.planty.db.entity.ConsultingBooking;
@@ -21,20 +22,38 @@ import static com.planty.common.util.LogCurrent.START;
 @Service
 @RequiredArgsConstructor
 public class GmBookingServiceImpl implements GmBookingService {
+
     private final GmInfoRepository gmInfoRepository;
     private final ConsultingBookingRepository consultingBookingRepository;
+    private final SubscribeProductRepository subscribeProductRepository;
 
     @Override // 그린메이트 예약 조회
-    public List<BookingResponse> getGmBooking() {
+    public List<GmBookingResponse> getGmBooking(Long spid) {
         log.info(logCurrent(getClassName(), getMethodName(), START));
         Long gid = SecurityUtil.getCurrentGid();
         GmInfo gm = gmInfoRepository.findByGid(gid)
                 .orElseThrow(() -> new NullPointerException(ExceptionHandler.GM_NOT_FOUND));
+        boolean isSpid = true;
+        if (spid == null) {
+            isSpid = false;
+        }
+        if (isSpid) {   // 유효한 spid에 대한 요청인지 확인
+            subscribeProductRepository.findBySpid(spid)
+                    .orElseThrow(() -> new NullPointerException(ExceptionHandler.PRODUCT_NOT_FOUND));
+        }
 
-        List<BookingResponse> bookingList = new ArrayList<>();
-        List<ConsultingBooking> list = consultingBookingRepository.findByGid(gm);
+        List<GmBookingResponse> bookingList = new ArrayList<>();
+        List<ConsultingBooking> list = consultingBookingRepository.findByGidAndActiveAndCancel(gm, false, false);
+//        if (spid == null) {
+//            list = consultingBookingRepository.findByGid(gm);
+//        } else {
+//            list = consultingBookingRepository.findByGid(gm);
+//        }
         for (ConsultingBooking item : list) {
-            BookingResponse booking = BookingResponse.builder()
+            if (isSpid && item.getSid().getSpid().getSpid() != spid) {
+                continue;
+            }
+            GmBookingResponse booking = GmBookingResponse.builder()
                     .sid(item.getSid().getSid())
                     .cid(item.getCid())
                     .title(item.getSid().getSpid().getName())
