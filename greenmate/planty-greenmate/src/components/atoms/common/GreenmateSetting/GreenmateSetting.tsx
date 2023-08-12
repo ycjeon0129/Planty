0ba77f734c761eb12ff3f24be86eab443464aeec
Gold-gyu <lkg2097@naver.com>
2023-08-12 12:53:02 +0900

    [FE/GM] Chore : 그린메이트 설정 완성(메뉴이름만 정해서 넣어주기)

--- greenmate/planty-greenmate/src/components/atoms/common/GreenmateSetting/GreenmateSetting.tsx
+++ greenmate/planty-greenmate/src/components/atoms/common/GreenmateSetting/GreenmateSetting.tsx
@@ -3,11 +3,11 @@ import './GreenmateSetting.scss';
 import NextIcon from 'assets/icons/Next.svg';
 // import { ISettingMenu } from 'types/base/dummy';
 
-function GreenmateSetting() {
+function GreenmateSetting({ settingMenu }: { settingMenu: string }) {
 	// ISettingMenu[]가 아닌 ISettingMenu로 수정
 	return (
 		<div className="greenmate-setting-box">
-			<div className="gray-text">다시하기</div>
+			<div className="gray-text">{settingMenu}</div>
 			<div>
 				<img src={NextIcon} alt="꺽새" />
 			</div>
--- greenmate/planty-greenmate/src/pages/Setting/SettingPage.tsx
+++ greenmate/planty-greenmate/src/pages/Setting/SettingPage.tsx
@@ -3,14 +3,17 @@ import SettingPageLayout from 'components/layout/page/Setting/SettingPageLayout/
 import React from 'react';
 import Profile from 'components/atoms/common/Profile/Profile';
 import GreenmateSetting from 'components/atoms/common/GreenmateSetting/GreenmateSetting';
-// 세팅 메뉴 데이터 가져오기
-// const settingData: string[] = ['이용약관', '개인정보 처리방침', '자주 묻는 질문', '버젼 정보'];
+
+const settingData: string[] = ['이용약관', '개인정보 처리방침', '자주 묻는 질문', '버젼 정보'];
+
 function SettingPage() {
 	return (
 		<SettingPageLayout>
 			<PageHeader text="설정" />
 			<Profile />
-			<GreenmateSetting />
+			{settingData.map((title) => (
+				<GreenmateSetting key={title} settingMenu={title} />
+			))}
 		</SettingPageLayout>
 	);
 }
