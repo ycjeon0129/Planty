cddbc681c735f12ac9c6f099bfc927b77d3e9b78
JeongHun <52409864+jeonghun98@users.noreply.github.com>
2023-08-13 01:22:22 +0900

    [BE] Fix : booking, subscribe api에서 post, delete 오류 수정

--- backend/planty-service/src/main/java/com/planty/api/booking/response/BookingResponse.java
+++ backend/planty-service/src/main/java/com/planty/api/booking/response/BookingResponse.java
@@ -23,4 +23,7 @@ public class BookingResponse {
     @NotNull
     private String user; // 사용자 이름
 
+    private Boolean cancel; // 취소여부. 취소(1), 미취소(0)
+
+    private Boolean active;  // 실행여부. 실행(1), 미실행(0)
 }
