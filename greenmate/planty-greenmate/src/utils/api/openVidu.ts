5a6c6c47b5d206a010155e48f8993e84a3bead11
임희선 <gmltjs4107@gmail.com>
2023-08-14 00:43:34 +0900

    [FE/GM] Chore : (임시) OpenVidu 우리 서버와 연결

--- greenmate/planty-greenmate/src/utils/api/openVidu.ts
+++ greenmate/planty-greenmate/src/utils/api/openVidu.ts
@@ -1,10 +1,8 @@
 import { openviduInstance } from './instance';
 
-const APPLICATION_SERVER_URL = process.env.REACT_APP_OPEN_VIDU_SERVER_URL ? '' : 'https://demos.openvidu.io/';
-
 export const createToken = async (sessionId: string) => {
 	const response = await openviduInstance.post(
-		`${APPLICATION_SERVER_URL}api/sessions/${sessionId}/connections`,
+		`/sessions/${sessionId}/connections`,
 		{},
 		{
 			headers: { 'Content-Type': 'application/json' },
@@ -13,17 +11,18 @@ export const createToken = async (sessionId: string) => {
 	return response.data; // The token
 };
 
-export const createSession = async (sessionId: string) => {
-	const response = await openviduInstance.post(
-		`${APPLICATION_SERVER_URL}api/sessions`,
-		{ customSessionId: sessionId },
-		{
-			headers: { 'Content-Type': 'application/json' },
-		},
-	);
-	return response.data; // The sessionId
-};
+// export const createSession = async (sessionId: string) => {
+// 	const response = await openviduInstance.post(
+// 		`/sessions`,
+// 		{ customSessionId: sessionId },
+// 		{
+// 			headers: { 'Content-Type': 'application/json' },
+// 		},
+// 	);
+// 	return response.data; // The sessionId
+// };
 
-export const getToken = async (sessionId: string) => {
-	return createToken(await createSession(sessionId));
+export const getToken = async () => {
+	// TODO : 백엔드에 요청해서 sessionId 가져와야 함
+	return createToken('ses_S584w5wJDZ');
 };
