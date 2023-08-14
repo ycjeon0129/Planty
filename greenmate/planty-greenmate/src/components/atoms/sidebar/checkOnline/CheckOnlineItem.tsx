aa579601f4f12d6a68dad250c46adcd9e4728fa2
전인혁 <wjs_5025@naver.com>
2023-08-14 22:04:13 +0900

    [FE/GM] Feat : 활동관리 토글 구현

--- greenmate/planty-greenmate/src/components/atoms/sidebar/checkOnline/CheckOnlineItem.tsx
+++ greenmate/planty-greenmate/src/components/atoms/sidebar/checkOnline/CheckOnlineItem.tsx
@@ -4,13 +4,14 @@ import './CheckOnlineItem.scss';
 import { useRecoilState } from 'recoil';
 import { activityState } from 'recoil/auth';
 import { Switch } from '@mui/material';
-import { saveActivityStateApi } from 'utils/api/auth';
+import { findActivityStateApi, saveActivityStateApi } from 'utils/api/auth';
 import { toast } from 'react-hot-toast';
 
 function CheckOnline() {
-	const [activity] = useRecoilState(activityState);
+	const [activity, setActivity] = useRecoilState(activityState);
 	const [message, setMessage] = useState('');
 
+	// 활동 관련 메시지를 변경하는 함수
 	const changeMessage = (state: boolean) => {
 		if (state) {
 			setMessage('현재 활동 중 입니다. \n모든 컨설팅 요청을 수신합니다.');
@@ -19,12 +20,14 @@ function CheckOnline() {
 		}
 	};
 
+	// 토글 스위치 클릭 시, 서버와 로컬의 활동 상태 변경
 	const changeActivityState = async () => {
 		try {
 			const response = await saveActivityStateApi();
 			if (response.status === 200) {
 				changeMessage(response.data.active);
-				console.log('활동상태 변경', response.data.active);
+				setActivity(response.data.active);
+				console.log('활동상태 변경', response.data);
 				toast.success('활동 상태 변경 완료😀');
 			}
 		} catch (error) {
@@ -33,16 +36,29 @@ function CheckOnline() {
 		}
 	};
 
+	// 최초 컴포넌트 마운트 시, 서버에서 내 활동상태를 받아와서 반영
+	const initActivityState = async () => {
+		try {
+			const response = await findActivityStateApi();
+			if (response.status === 200) {
+				changeMessage(response.data.active);
+				setActivity(response.data.active);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	useEffect(() => {
-		changeMessage(activity);
-	}, [activity]);
+		initActivityState();
+	}, []);
 
 	return (
 		<div className="check-online-outer-box">
 			<div className="check-online-box">
 				<div className="active-management">
 					<div className="bold-text">활동 관리</div>
-					<Switch defaultChecked={activity} onClick={changeActivityState} />
+					<Switch checked={activity} onChange={changeActivityState} inputProps={{ 'aria-label': 'controlled' }} />
 				</div>
 				<div>
 					<div className="gray-text">{message}</div>
