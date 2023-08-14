18bbdf51d2ab27bee4011ddebb649ef71186fb28
jeonghun98 <wjdgns7040@gmail.com>
2023-08-14 13:30:46 +0900

    [BE] Chore : 구독, 예약 리스트 정렬 (날짜, 시간)

--- backend/planty-service/src/main/java/com/planty/db/repository/ViewUserConsultingRepository.java
+++ backend/planty-service/src/main/java/com/planty/db/repository/ViewUserConsultingRepository.java
@@ -3,6 +3,7 @@ package com.planty.db.repository;
 import com.planty.db.entity.ConsultingBooking;
 import com.planty.db.entity.UserInfo;
 import com.planty.db.entity.ViewUserConsulting;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,10 +12,10 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface ViewUserConsultingRepository extends JpaRepository<ViewUserConsulting, Long> {
-    List<ViewUserConsulting> findByUid(Long userId);
-    List<ViewUserConsulting> findByGid(Long gid);
-    List<ViewUserConsulting> findByGidAndSpid(Long gid, Long spid);
-    List<ViewUserConsulting> findByUidAndSid(Long uid, Long sid);
+    List<ViewUserConsulting> findByUid(Long userId, Sort sort);
+    List<ViewUserConsulting> findByGid(Long gid, Sort sort);
+    List<ViewUserConsulting> findByGidAndSpid(Long gid, Long spid, Sort sort);
+    List<ViewUserConsulting> findByUidAndSid(Long uid, Long sid, Sort sort);
     @Query(value = "select cb from ConsultingBooking cb where cb.cid in (select max(cid) from ConsultingBooking group by sid) and cb.uid = :uid")
     List<ConsultingBooking> findConsultingBooking(@Param("uid") UserInfo uid);
 
