c7a406a0ae46e51a43a98f0e81c59a41ecb6b671
전인혁 <wjs_5025@naver.com>
2023-08-16 00:17:45 +0900

    [FE] Chore : 불필요한 api 제거

--- frontend/src/utils/api/openVidu.ts
+++ frontend/src/utils/api/openVidu.ts
@@ -1,35 +1,12 @@
-import { ISessionInfo } from 'types/common/request';
+import { IEmergencySessionInfo, ISubscribeSessionInfo } from 'types/common/request';
 import { openviduInstance } from './instance';
-
-// export const createToken = async (sessionId: string) => {
-// 	const response = await openviduInstance.post(
-// 		`/sessions/${sessionId}/connections`,
-// 		// `${APPLICATION_SERVER_URL}api/sessions/${sessionId}/connections`,
-// 		{},
-// 		{
-// 			headers: { 'Content-Type': 'application/json' },
-// 		},
-// 	);
-// 	return response.data; // The token
-// };
-
-// export const createSession = async () => {
-// 	const response = await openviduInstance.post(`/sessions`, {
-// 		headers: { 'Content-Type': 'application/json' },
-// 	});
-// 	return response.data; // The sessionId
-// };
-
-// export const getToken = async () => {
-// 	return createToken(await createSession());
-// };
 
 export const createEmergencySessionIdApi = async (type: number) => {
 	const response = await openviduInstance.post(`/emergencies/${type}`);
 	return response;
 };
 
-export const createEmergencyConnectionApi = async (sessionInfo: ISessionInfo) => {
+export const createEmergencyConnectionApi = async (sessionInfo: IEmergencySessionInfo) => {
 	const body = {
 		...sessionInfo,
 	};
@@ -39,10 +16,11 @@ export const createEmergencyConnectionApi = async (sessionInfo: ISessionInfo) =>
 
 export const createSubscribeSessionIdApi = async () => {
 	const response = await openviduInstance.post(`/consultings`);
+
 	return response;
 };
 
-export const createSubscribeConnectionApi = async (sessionInfo: ISessionInfo) => {
+export const createSubscribeConnectionApi = async (sessionInfo: ISubscribeSessionInfo) => {
 	const body = {
 		...sessionInfo,
 	};
