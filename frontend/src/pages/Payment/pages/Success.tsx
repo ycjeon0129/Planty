9e511590bc101649d1d1b0b389990900248069c7
임희선 <huiseon4107@gmail.com>
2023-08-16 05:43:51 +0900

    [FE] Feat : 구독 상품/응급실 티켓 구매 API 연결

--- frontend/src/pages/Payment/pages/Success.tsx
+++ frontend/src/pages/Payment/pages/Success.tsx
@@ -20,7 +20,11 @@ function Success() {
 
 	return (
 		<div className="complete-container">
-			{isLoading ? <PayLoadingPageLayout /> : <PayComplete price={state.price} />}
+			{isLoading && state ? (
+				<PayLoadingPageLayout />
+			) : (
+				<PayComplete price={state.product.price} spid={state.product.spid} />
+			)}
 		</div>
 	);
 }
