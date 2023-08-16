7d556245acdb6a57eea30f9d8095200b488fbf14
전윤철 <tlfldntm4444@naver.com>
2023-08-16 17:08:14 +0900

    [BE] Feat : BE 서버 타임존 변경

--- backend/planty-service/src/main/java/com/planty/PlantyServiceApplication.java
+++ backend/planty-service/src/main/java/com/planty/PlantyServiceApplication.java
@@ -2,12 +2,19 @@ package com.planty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 //import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //@EnableFeignClients
 @SpringBootApplication
 public class PlantyServiceApplication {
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PlantyServiceApplication.class, args);
 	}
