2b7616b396bd8e1ec6b9e42d417b52035ada80cb
jeonghun98 <wjdgns7040@gmail.com>
2023-08-16 14:48:58 +0900

    [BE] Feat : Embedded api 조회 구현

--- backend/planty-service/src/main/java/com/planty/api/embedded/service/EmbeddedServiceImpl.java
+++ backend/planty-service/src/main/java/com/planty/api/embedded/service/EmbeddedServiceImpl.java
@@ -2,6 +2,7 @@ package com.planty.api.embedded.service;
 
 import com.planty.api.booking.request.UserBookingRequest;
 import com.planty.api.embedded.repuest.EmbeddedRequest;
+import com.planty.api.embedded.response.UserSubscribeEmbeddedResponse;
 import com.planty.common.exception.handler.ExceptionHandler;
 import com.planty.common.util.SecurityUtil;
 import com.planty.db.entity.*;
@@ -9,6 +10,9 @@ import com.planty.db.repository.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.planty.common.util.LogCurrent.*;
 import static com.planty.common.util.LogCurrent.END;
@@ -20,7 +24,7 @@ public class EmbeddedServiceImpl implements EmbeddedService {
     private final UserInfoRepository userInfoRepository;
     private final UserSubscribeRepository userSubscribeRepository;
     private final PlantDataRepository plantDataRepository;
-    @Override // 사용자 컨설팅 등록
+    @Override // 임베디드 데이터 등록
     public boolean regEmbedded (EmbeddedRequest embeddedRequest){
         log.info(logCurrent(getClassName(), getMethodName(), START));
         String email = SecurityUtil.getCurrentUserEmail();
@@ -40,5 +44,30 @@ public class EmbeddedServiceImpl implements EmbeddedService {
         plantDataRepository.save(plantData);
         log.info(logCurrent(getClassName(), getMethodName(), END));
         return true;
+    }
+
+    @Override // 임베디드 데이터 조회
+    public List<UserSubscribeEmbeddedResponse> getEmbedded(Long sid) {
+        log.info(logCurrent(getClassName(), getMethodName(), START));
+        String email = SecurityUtil.getCurrentUserEmail();
+        UserInfo user = userInfoRepository.findByUserEmail(email)
+                .orElseThrow(() -> new NullPointerException(ExceptionHandler.USER_NOT_FOUND));
+
+        UserSubscribe subscribe = userSubscribeRepository.findByUidAndSid(user,sid)
+                .orElseThrow(() -> new NullPointerException(ExceptionHandler.USER_SID_NOT_FOUND));
+
+        List<PlantDataAvgInterface> list = plantDataRepository.findDateAvgByArduinoId(subscribe.getArduinoId());
+        List<UserSubscribeEmbeddedResponse> embeddedList = new ArrayList<>();
+        for(PlantDataAvgInterface item : list) {
+            UserSubscribeEmbeddedResponse embedded = UserSubscribeEmbeddedResponse.builder()
+                    .date(item.getDate().substring(2))
+                    .temp(Math.round((item.getTemp()*100) / 100))
+                    .humidity(Math.round((item.getHumidity()*100) / 100))
+                    .soil(Math.round((item.getSoil()*100) / 100))
+                    .build();
+            embeddedList.add(embedded);
+        }
+        log.info(logCurrent(getClassName(), getMethodName(), END));
+        return embeddedList;
     }
 }
--- backend/planty-service/src/main/java/com/planty/db/repository/PlantDataRepository.java
+++ backend/planty-service/src/main/java/com/planty/db/repository/PlantDataRepository.java
@@ -2,10 +2,14 @@ package com.planty.db.repository;
 
 import com.planty.db.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface PlantDataRepository extends JpaRepository<PlantData, Long> {
     List<PlantData> findByArduinoId(Integer arduinoId);
+    @Query(value = "select pd.date as date, avg(pd.temp) as temp, avg(pd.humidity) as humidity, avg(pd.soil) as soil from PlantData pd where pd.arduinoId  = :arduinoId GROUP BY pd.date")
+    List<PlantDataAvgInterface> findDateAvgByArduinoId(@Param("arduinoId") Integer arduinoId);
 }
